c5bdbc157163545668289b7efc38b1c93825ca50
Aiden Keating <akeating@redhat.com>
2020-09-13 20:30:12 +0100

    add cluster creation command to avoid rest api requirement
    currently, the cluster service is not accessible through the rest
    api that the cli 'serve' command provides. this makes manual
    testing of changes to the cluster creation logic difficult.
    
    this commit exposes an additional command on the cli for cluster
    service interaction. this allows the cluster service to be tested
    manually without exposing a rest api route. this command leverages
    the environment of the rest api, meaning database connections etc
    are still used.
    
    verification:
    - ensure ocm and aws tokens have been exposed per the readme
    - run 'make binary'
    - run './managed-services-api cluster create'
    - ensure a cluster is created and the command completes with an
      ok exit code
    - ensure the readme additions are accurate

---
+++ cmd/managed-services-api/cluster/create.go
@@ -0,0 +1,58 @@
+package cluster
+
+import (
+	"github.com/golang/glog"
+	"github.com/spf13/cobra"
+	"gitlab.cee.redhat.com/service/managed-services-api/cmd/managed-services-api/environments"
+	"gitlab.cee.redhat.com/service/managed-services-api/pkg/api"
+	"gitlab.cee.redhat.com/service/managed-services-api/pkg/errors"
+	"gitlab.cee.redhat.com/service/managed-services-api/pkg/services"
+)
+
+// NewCreateCommand creates a new command for creating clusters.
+func NewCreateCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "create",
+		Short: "Create a new managed-services-api cluster",
+		Long:  "Create a new managed-services-api cluster.",
+		Run:   runCreate,
+	}
+	err := environments.Environment().AddFlags(cmd.PersistentFlags())
+	if err != nil {
+		glog.Fatalf("Unable to add environment flags to serve command: %s", err.Error())
+	}
+
+	cmd.Flags().String("region", "eu-west-1", "Cluster region ID")
+	cmd.Flags().String("provider", "aws", "Cluster provider")
+
+	return cmd
+}
+
+func runCreate(cmd *cobra.Command, args []string) {
+	region, err := cmd.Flags().GetString("region")
+	if err != nil {
+		glog.Fatalf("Unable to parse region flag: %s", err.Error())
+	}
+	provider, err := cmd.Flags().GetString("provider")
+	if err != nil {
+		glog.Fatalf("Unable to parse provider flag: %s", err.Error())
+	}
+
+	if err = environments.Environment().Initialize(); err != nil {
+		glog.Fatalf("Unable to initialize environment: %s", err.Error())
+	}
+
+	env := environments.Environment()
+
+	clusterService := services.NewClusterService(env.DBFactory, env.Clients.OCM.Connection, env.Config.AWS)
+
+	cluster, err := clusterService.Create(&api.Cluster{
+		CloudProvider: provider,
+		Region:        region,
+	})
+	// see https://yourbasic.org/golang/gotcha-why-nil-error-not-equal-nil/ for the reason for casting.
+	if err != (*errors.ServiceError)(nil) {
+		glog.Fatalf("Unable to create cluster: %s", err.Error())
+	}
+	glog.Infof("Cluster %s created", cluster.ID())
+}
