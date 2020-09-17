b9448d7c9592b355ff3752ad9d873bc84fa81387
Aiden Keating <akeating@redhat.com>
2020-09-17 15:51:03 +0100

    add integration tests for kafka get endpoint
    currently the GET /kafka/{id} endpoint is not tested via
    integration tests.
    
    this commit adds an initial set of integration tests that can be
    executed against the service
    
    verification:
    - run 'make test/run'

--- pkg/api/presenters/kind.go
+++ pkg/api/presenters/kind.go
@@ -5,12 +5,19 @@ import (
 	"gitlab.cee.redhat.com/service/managed-services-api/pkg/errors"
 )
 
+const (
+	// KindKafka is a string identifier for the type api.KafkaRequest
+	KindKafka = "Kafka"
+	// KindError is a string identifier for the type api.ServiceError
+	KindError = "Error"
+)
+
 func ObjectKind(i interface{}) string {
 	switch i.(type) {
 	case api.KafkaRequest, *api.KafkaRequest:
-		return "Kafka"
+		return KindKafka
 	case errors.ServiceError, *errors.ServiceError:
-		return "Error"
+		return KindError
 	default:
 		return ""
 	}
--- test/integration/kafkas_test.go
+++ test/integration/kafkas_test.go
@@ -5,9 +5,16 @@ import (
 	"github.com/go-resty/resty"
 	. "github.com/onsi/gomega"
 	"gitlab.cee.redhat.com/service/managed-services-api/pkg/api/openapi"
+	"gitlab.cee.redhat.com/service/managed-services-api/pkg/api/presenters"
+	"gitlab.cee.redhat.com/service/managed-services-api/pkg/clusterservicetest"
+	"gitlab.cee.redhat.com/service/managed-services-api/pkg/services"
 	"gitlab.cee.redhat.com/service/managed-services-api/test"
 	"net/http"
 	"testing"
+)
+
+const (
+	mockKafkaName = "test"
 )
 
 func TestKafkaPost(t *testing.T) {
@@ -19,9 +26,9 @@ func TestKafkaPost(t *testing.T) {
 	// POST responses per openapi spec: 201, 409, 500
 
 	k := openapi.KafkaRequest{
-		Region:        "eu-west-1",
-		CloudProvider: "aws",
-		Name:          "test",
+		Region:        clusterservicetest.MockClusterRegion,
+		CloudProvider: clusterservicetest.MockClusterCloudProvider,
+		Name:          mockKafkaName,
 	}
 
 	// 202 Accepted
@@ -29,7 +36,7 @@ func TestKafkaPost(t *testing.T) {
 	Expect(err).NotTo(HaveOccurred(), "Error posting object:  %v", err)
 	Expect(resp.StatusCode).To(Equal(http.StatusAccepted))
 	Expect(kafka.Id).NotTo(BeEmpty(), "Expected ID assigned on creation")
-	Expect(kafka.Kind).To(Equal("Kafka"))
+	Expect(kafka.Kind).To(Equal(presenters.KindKafka))
 	Expect(kafka.Href).To(Equal(fmt.Sprintf("/api/managed-services-api/v1/kafkas/%s", kafka.Id)))
 
 	// 400 bad request. posting junk json is one way to trigger 400.
@@ -41,4 +48,38 @@ func TestKafkaPost(t *testing.T) {
 		Post(h.RestURL("/kafkas"))
 
 	Expect(restyResp.StatusCode()).To(Equal(http.StatusBadRequest))
+}
+
+func TestKafkaGet(t *testing.T) {
+	h, client := test.RegisterIntegration(t)
+
+	account := h.NewRandAccount()
+	ctx := h.NewAuthenticatedContext(account)
+
+	k := openapi.KafkaRequest{
+		Region:        clusterservicetest.MockClusterRegion,
+		CloudProvider: clusterservicetest.MockClusterCloudProvider,
+		Name:          mockKafkaName,
+	}
+
+	seedKafka, _, err := client.DefaultApi.ApiManagedServicesApiV1KafkasPost(ctx, k)
+	if err != nil {
+		t.Fatalf("failed to create seeded kafka request: %s", err.Error())
+	}
+
+	// 200 OK
+	kafka, resp, err := client.DefaultApi.ApiManagedServicesApiV1KafkasIdGet(ctx, seedKafka.Id)
+	Expect(err).NotTo(HaveOccurred(), "Error occurred when attempting to get kafka request:  %v", err)
+	Expect(resp.StatusCode).To(Equal(http.StatusOK))
+	Expect(kafka.Id).NotTo(BeEmpty(), "Expected ID assigned on creation")
+	Expect(kafka.Kind).To(Equal(presenters.KindKafka))
+	Expect(kafka.Href).To(Equal(fmt.Sprintf("/api/managed-services-api/v1/kafkas/%s", kafka.Id)))
+	Expect(kafka.Region).To(Equal(clusterservicetest.MockClusterRegion))
+	Expect(kafka.CloudProvider).To(Equal(clusterservicetest.MockClusterCloudProvider))
+	Expect(kafka.Name).To(Equal(mockKafkaName))
+	Expect(kafka.Status).To(Equal(services.KafkaRequestStatusAccepted.String()))
+
+	// 404 Not Found
+	kafka, resp, err = client.DefaultApi.ApiManagedServicesApiV1KafkasIdGet(ctx, fmt.Sprintf("not-%s", seedKafka.Id))
+	Expect(resp.StatusCode).To(Equal(http.StatusNotFound))
 }
