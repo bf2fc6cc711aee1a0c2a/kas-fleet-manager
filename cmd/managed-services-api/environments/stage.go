1d8265f5498b396ce6551735c78b69e314fdbf82
Jameel Briones <jbriones@redhat.com>
2020-09-22 10:17:32 +0100

    add stage environment
    adds a new environment for running the service in stage.
    
    JIRA: https://issues.redhat.com/browse/MGDSTRM-163

---
+++ cmd/managed-services-api/environments/stage.go
@@ -0,0 +1,22 @@
+package environments
+
+import (
+	"gitlab.cee.redhat.com/service/managed-services-api/pkg/db"
+)
+
+var stageConfigDefaults map[string]string = map[string]string{
+	"ocm-base-url":    "https://api.stage.openshift.com",
+	"enable-ocm-mock": "false",
+}
+
+func loadStage(env *Env) error {
+	env.DBFactory = db.NewConnectionFactory(env.Config.Database)
+
+	err := env.LoadClients()
+	if err != nil {
+		return err
+	}
+	env.LoadServices()
+
+	return env.InitializeSentry()
+}
