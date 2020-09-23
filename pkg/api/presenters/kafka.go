18e448a3a812856ce0e764ce2a3fbc6f77d558aa
David Ffrench <davidffrench@gmail.com>
2020-09-23 22:12:01 +0100

    Adding bootstrap server host generation and override

--- pkg/api/presenters/kafka.go
+++ pkg/api/presenters/kafka.go
@@ -10,29 +10,31 @@ func ConvertKafkaRequest(kafkaRequest openapi.KafkaRequest) *api.KafkaRequest {
 		Meta: api.Meta{
 			ID: kafkaRequest.Id,
 		},
-		Region:        kafkaRequest.Region,
-		Name:          kafkaRequest.Name,
-		ClusterID:     kafkaRequest.ClusterID,
-		CloudProvider: kafkaRequest.CloudProvider,
-		MultiAZ:       kafkaRequest.MultiAz,
-		Owner:         kafkaRequest.Owner,
+		Region:              kafkaRequest.Region,
+		Name:                kafkaRequest.Name,
+		ClusterID:           kafkaRequest.ClusterID,
+		CloudProvider:       kafkaRequest.CloudProvider,
+		MultiAZ:             kafkaRequest.MultiAz,
+		Owner:               kafkaRequest.Owner,
+		BootstrapServerHost: kafkaRequest.BootstrapServerHost,
 	}
 }
 
 func PresentKafkaRequest(kafkaRequest *api.KafkaRequest) openapi.KafkaRequest {
 	reference := PresentReference(kafkaRequest.ID, kafkaRequest)
 	return openapi.KafkaRequest{
-		Id:            reference.Id,
-		Kind:          reference.Kind,
-		Href:          reference.Href,
-		Region:        kafkaRequest.Region,
-		Name:          kafkaRequest.Name,
-		ClusterID:     kafkaRequest.ClusterID,
-		CloudProvider: kafkaRequest.CloudProvider,
-		MultiAz:       kafkaRequest.MultiAZ,
-		Owner:         kafkaRequest.Owner,
-		Status:        kafkaRequest.Status,
-		CreatedAt:     kafkaRequest.CreatedAt,
-		UpdatedAt:     kafkaRequest.UpdatedAt,
+		Id:                  reference.Id,
+		Kind:                reference.Kind,
+		Href:                reference.Href,
+		Region:              kafkaRequest.Region,
+		Name:                kafkaRequest.Name,
+		ClusterID:           kafkaRequest.ClusterID,
+		CloudProvider:       kafkaRequest.CloudProvider,
+		MultiAz:             kafkaRequest.MultiAZ,
+		Owner:               kafkaRequest.Owner,
+		BootstrapServerHost: kafkaRequest.BootstrapServerHost,
+		Status:              kafkaRequest.Status,
+		CreatedAt:           kafkaRequest.CreatedAt,
+		UpdatedAt:           kafkaRequest.UpdatedAt,
 	}
 }
--- pkg/db/converters/kafka_request.go
+++ pkg/db/converters/kafka_request.go
@@ -4,16 +4,19 @@ import "gitlab.cee.redhat.com/service/managed-services-api/pkg/api"
 
 func ConvertKafkaRequest(request *api.KafkaRequest) []map[string]interface{} {
 	return []map[string]interface{}{
-		{"id": request.ID},
-		{"region": request.Region},
-		{"cloud_provider": request.CloudProvider},
-		{"multi_az": request.MultiAZ},
-		{"cluster_name": request.Name},
-		{"status": request.Status},
-		{"owner": request.Owner},
-		{"cluster_id": request.ClusterID},
-		{"created_at": request.Meta.CreatedAt},
-		{"updated_at": request.Meta.UpdatedAt},
-		{"deleted_at": request.Meta.DeletedAt},
+		{
+			"id":                    request.ID,
+			"region":                request.Region,
+			"cloud_provider":        request.CloudProvider,
+			"multi_az":              request.MultiAZ,
+			"name":                  request.Name,
+			"status":                request.Status,
+			"owner":                 request.Owner,
+			"cluster_id":            request.ClusterID,
+			"bootstrap_server_host": request.BootstrapServerHost,
+			"created_at":            request.Meta.CreatedAt,
+			"updated_at":            request.Meta.UpdatedAt,
+			"deleted_at":            request.Meta.DeletedAt,
+		},
 	}
 }
