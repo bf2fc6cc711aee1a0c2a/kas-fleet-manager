990851f13e60f7462a76ce7ac7116e96a196dcca
Pawel Paszki <ppaszki@redhat.com>
2020-10-01 09:31:35 +0000

    fixed integration tests running in integration environment

--- test/mocks/api_server.go
+++ test/mocks/api_server.go
@@ -1,6 +1,7 @@
 package mocks
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -21,16 +22,34 @@ const (
 	// MockConfigurableServerSyncsetID default mock syncset id used in the mock ocm server
 	MockConfigurableServerSyncsetID = "ext-8a41f783-b5e4-4692-a7cd-c0b9c8eeede9"
 
+	// MockConfigurableServerIngressID default mock ingress id used in the mock ocm server
+	MockConfigurableServerIngressID = "s1h5"
+
+	// MockConfigurableServerIngressDNS default mock ingress dns used in the mock ocm server
+	MockConfigurableServerIngressDNS = "apps.ms-btq2d1h8d3b1.b3k3.s1.devshift.org"
+
+	// MockConfigurableServerIngressHref default mock ingress HREF used in the mock ocm server
+	MockConfigurableServerIngressHref = "/api/clusters_mgmt/v1/clusters/000/ingresses/i8y1"
+
+	// MockConfigurableServerIngressListening default mock ingress listening used in the mock ocm server
+	MockConfigurableServerIngressListening = "external"
+
 	// EndpointPathClusters ocm clusters management service clusters endpoint
 	EndpointPathClusters = "/api/clusters_mgmt/v1/clusters"
+	// EndpointPathClusters ocm clusters management service clusters endpoint
+	EndpointPathCluster = "/api/clusters_mgmt/v1/clusters/000"
 	// EndpointPathSyncsets ocm clusters management service syncset endpoint
-	EndpointPathSyncsets = "/api/clusters_mgmt/v1/clusters/external_configuration/syncsets"
+	EndpointPathSyncsets = "/api/clusters_mgmt/v1/clusters/000/external_configuration/syncsets"
+	// EndpointPathIngresses ocm cluster management ingress endpoint
+	EndpointPathIngresses = "/api/clusters_mgmt/v1/clusters/000/ingresses"
 )
 
 var (
+	EndpointClusterGet         = Endpoint{EndpointPathCluster, http.MethodGet}
 	EndpointClustersGet        = Endpoint{EndpointPathClusters, http.MethodGet}
 	EndpointClustersPost       = Endpoint{EndpointPathClusters, http.MethodPost}
 	EndpointClusterSyncsetPost = Endpoint{EndpointPathSyncsets, http.MethodPost}
+	EndpointClusterIngressGet  = Endpoint{EndpointPathIngresses, http.MethodGet}
 )
 
 // Endpoint is a wrapper around an endpoint and the method used to interact with that endpoint e.g. GET /clusters
@@ -61,6 +80,11 @@ func NewMockConfigurableServerBuilder() *MockConfigurableServerBuilder {
 }
 
 // SetClustersPostResponse set a mock response cluster or error for the POST /api/clusters_mgmt/v1/clusters endpoint
+func (b *MockConfigurableServerBuilder) SetClusterGetResponse(cluster *clustersmgmtv1.Cluster, err *ocmErrors.ServiceError) {
+	b.handlerRegister[EndpointClusterGet] = buildMockRequestHandler(cluster, err)
+}
+
+// SetClustersPostResponse set a mock response cluster or error for the POST /api/clusters_mgmt/v1/clusters endpoint
 func (b *MockConfigurableServerBuilder) SetClustersPostResponse(cluster *clustersmgmtv1.Cluster, err *ocmErrors.ServiceError) {
 	b.handlerRegister[EndpointClustersPost] = buildMockRequestHandler(cluster, err)
 }
@@ -70,9 +94,14 @@ func (b *MockConfigurableServerBuilder) SetClustersGetResponse(cluster *clusters
 	b.handlerRegister[EndpointClustersGet] = buildMockRequestHandler(cluster, err)
 }
 
-// SetClusterSyncsetPostResponse set a mock response syncset or error for the POST /api/clusters_mgmt/v1/clusters/{id}/external_configuration/syncsets endpoint
+// SetClusterSyncsetPostResponse set a mock response syncset or error for the POST /api/clusters_mgmt/v1/clusters/{id}/syncsets endpoint
 func (b *MockConfigurableServerBuilder) SetClusterSyncsetPostResponse(syncset *clustersmgmtv1.Syncset, err *ocmErrors.ServiceError) {
 	b.handlerRegister[EndpointClusterSyncsetPost] = buildMockRequestHandler(syncset, err)
+}
+
+// SetIngressGetResponse set a mock response ingress or error for the GET /api/clusters_mgmt/v1/clusters/{id}/ingresses endpoint
+func (b *MockConfigurableServerBuilder) SetClusterIngressGetResponse(ingress *clustersmgmtv1.Ingress, err *ocmErrors.ServiceError) {
+	b.handlerRegister[EndpointClusterIngressGet] = buildMockRequestHandler(ingress, err)
 }
 
 // Build builds the mock ocm api server using the endpoint handlers that have been set in the builder
@@ -97,12 +126,19 @@ func getDefaultHandlerRegister() (HandlerRegister, error) {
 	if err != nil {
 		return nil, err
 	}
+	mockIngressList, err := clustersmgmtv1.NewIngressList().Items(
+		clustersmgmtv1.NewIngress().ID(MockConfigurableServerIngressID).DNSName(MockConfigurableServerIngressDNS).Default(true).Listening(MockConfigurableServerIngressListening).HREF(MockConfigurableServerIngressHref)).Build()
+	if err != nil {
+		return nil, err
+	}
 	// define a list of default endpoints and handlers in the mock ocm api server, when new endpoints are used in the
 	// managed-services-api service, a default ocm response should also be added here
 	return HandlerRegister{
+		EndpointClusterGet:         buildMockRequestHandler(mockCluster, nil),
 		EndpointClustersGet:        buildMockRequestHandler(mockCluster, nil),
 		EndpointClustersPost:       buildMockRequestHandler(mockCluster, nil),
 		EndpointClusterSyncsetPost: buildMockRequestHandler(mockSyncset, nil),
+		EndpointClusterIngressGet:  buildMockRequestHandler(mockIngressList, nil),
 	}, nil
 }
 
@@ -135,8 +171,56 @@ func marshalOCMType(t interface{}, w io.Writer) error {
 		return clustersmgmtv1.MarshalCluster(t.(*clustersmgmtv1.Cluster), w)
 	case *clustersmgmtv1.Syncset:
 		return clustersmgmtv1.MarshalSyncset(t.(*clustersmgmtv1.Syncset), w)
+	case *clustersmgmtv1.Ingress:
+		return clustersmgmtv1.MarshalIngress(t.(*clustersmgmtv1.Ingress), w)
+	case []*clustersmgmtv1.Ingress:
+		return clustersmgmtv1.MarshalIngressList(t.([]*clustersmgmtv1.Ingress), w)
+	// for any <type>List ocm type we'll need to follow this pattern to ensure the array of objects
+	// is wrapped with an OCMList object
+	case *clustersmgmtv1.IngressList:
+		ocmList, err := NewOCMList().WithItems(t.(*clustersmgmtv1.IngressList).Slice())
+		if err != nil {
+			return err
+		}
+		return json.NewEncoder(w).Encode(ocmList)
+	case *ocmList:
+		return json.NewEncoder(w).Encode(t)
 	case *ocmErrors.ServiceError:
 		return json.NewEncoder(w).Encode(t.(*ocmErrors.ServiceError).AsOpenapiError(""))
 	}
 	return errors.New(fmt.Sprintf("could not recognise type %s in ocm type marshaller", reflect.TypeOf(t).String()))
+}
+
+// basic wrapper to emulate the the ocm list types as they're private
+type ocmList struct {
+	HREF  *string         `json:"href"`
+	Link  bool            `json:"link"`
+	Items json.RawMessage `json:"items"`
+}
+
+func NewOCMList() *ocmList {
+	return &ocmList{
+		HREF:  nil,
+		Link:  false,
+		Items: nil,
+	}
+}
+
+func (l *ocmList) WithHREF(href string) *ocmList {
+	l.HREF = &href
+	return l
+}
+
+func (l *ocmList) WithLink(link bool) *ocmList {
+	l.Link = link
+	return l
+}
+
+func (l *ocmList) WithItems(items interface{}) (*ocmList, error) {
+	var b bytes.Buffer
+	if err := marshalOCMType(items, &b); err != nil {
+		return l, err
+	}
+	l.Items = b.Bytes()
+	return l, nil
 }
