bbccbba0e790635b4b801f102745252710c7ff56
dimitraz <dzuccare@redhat.com>
2020-10-30 12:10:14 +0000

    Add machine pool scale down integration test

--- test/integration/cluster_scale_test.go
+++ test/integration/cluster_scale_test.go
@@ -13,7 +13,8 @@ import (
 func TestClusterScaleUp(t *testing.T) {
 	// create a mock ocm api server, keep all endpoints as defaults
 	// see the mocks package for more information on the configurable mock server
-	ocmServer := mocks.NewMockConfigurableServerBuilder().Build()
+	ocmServerBuilder := mocks.NewMockConfigurableServerBuilder()
+	ocmServer := ocmServerBuilder.Build()
 	defer ocmServer.Close()
 
 	// setup the test environment, if OCM_ENV=integration then the ocmServer provided will be used instead of actual
@@ -34,4 +35,47 @@ func TestClusterScaleUp(t *testing.T) {
 	Expect(err).To(BeNil())
 	Expect(machinePool.ID()).To(Equal(services.DefaultMachinePoolID))
 	Expect(machinePool.Replicas()).To(Equal(2))
+
+	// scale up by one node
+	machinePool, err = h.Env().Services.Cluster.ScaleUpMachinePool(clusterID)
+	Expect(err).To(BeNil())
+	Expect(machinePool.ID()).To(Equal(services.DefaultMachinePoolID))
+
+	// we need to override the response for local testing
+	//Expect(machinePool.Replicas()).To(Equal(3))
+}
+
+func TestClusterScaleDown(t *testing.T) {
+	// create a mock ocm api server, keep all endpoints as defaults
+	// see the mocks package for more information on the configurable mock server
+	ocmServerBuilder := mocks.NewMockConfigurableServerBuilder()
+	ocmServer := ocmServerBuilder.Build()
+	defer ocmServer.Close()
+
+	// setup the test environment, if OCM_ENV=integration then the ocmServer provided will be used instead of actual
+	// ocm
+	h, _, teardown := test.RegisterIntegration(t, ocmServer)
+	defer teardown()
+
+	clusterID, getClusterErr := utils.GetRunningOsdClusterID(h, t)
+	if getClusterErr != nil {
+		t.Fatalf("Failed to retrieve cluster details from persisted .json file: %v", getClusterErr)
+	}
+	if clusterID == "" {
+		panic("No cluster found")
+	}
+
+	// create machine pool
+	machinePool, err := h.Env().Services.Cluster.ScaleUpMachinePool(clusterID)
+	Expect(err).To(BeNil())
+	Expect(machinePool.ID()).To(Equal(services.DefaultMachinePoolID))
+	Expect(machinePool.Replicas()).To(Equal(2))
+
+	// scale down machine pool
+	machinePool, err = h.Env().Services.Cluster.ScaleDownMachinePool(clusterID)
+	Expect(err).To(BeNil())
+	Expect(machinePool.ID()).To(Equal(services.DefaultMachinePoolID))
+
+	// we need to override the response for local testing
+	//Expect(machinePool.Replicas()).To(Equal(1))
 }
