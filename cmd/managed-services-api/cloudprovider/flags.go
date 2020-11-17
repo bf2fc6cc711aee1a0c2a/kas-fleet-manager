fa604a534a414b097dea0adc0d4907487895806a
Abdul Hameed <ahameed@redhat.com>
2020-11-17 13:49:17 -0500

    Added a new endpoint for the supported cloud providers and regions
    Signed-off-by: Abdul Hameed <ahameed@redhat.com>

---
+++ cmd/managed-services-api/cloudprovider/flags.go
@@ -0,0 +1,6 @@
+package cloudprovider
+
+const (
+	// FlagID is a flag representing a provider Id like aws
+	FlagID = "id"
+)
---
+++ pkg/handlers/cloud_providers.go
@@ -0,0 +1,83 @@
+package handlers
+
+import (
+	"github.com/gorilla/mux"
+	"gitlab.cee.redhat.com/service/managed-services-api/pkg/api/openapi"
+	"gitlab.cee.redhat.com/service/managed-services-api/pkg/api/presenters"
+	"gitlab.cee.redhat.com/service/managed-services-api/pkg/errors"
+	"gitlab.cee.redhat.com/service/managed-services-api/pkg/services"
+	"net/http"
+)
+
+type cloudProvidersHandler struct {
+	service services.CloudProvidersService
+	config  services.ConfigService
+}
+
+func NewCloudProviderHandler(service services.CloudProvidersService, configService services.ConfigService) *cloudProvidersHandler {
+	return &cloudProvidersHandler{
+		service: service,
+		config:  configService,
+	}
+}
+
+func (h cloudProvidersHandler) ListCloudProviderRegions(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+
+	cfg := &handlerConfig{
+
+		Validate: []validate{
+			validateNotEmpty(&id, "id"),
+		},
+
+		Action: func() (i interface{}, serviceError *errors.ServiceError) {
+			cloudRegions, err := h.service.ListCloudProviderRegions(id)
+			if err != nil {
+				return nil, err
+			}
+			regionList := openapi.CloudRegionList{
+				Kind:  "CloudRegionList",
+				Total: int32(len(cloudRegions)),
+				Size:  int32(len(cloudRegions)),
+				Page:  int32(1),
+			}
+			for _, cloudRegion := range cloudRegions {
+				cloudRegion.Enabled = h.config.IsRegionSupportedForProvider(cloudRegion.CloudProvider, cloudRegion.Id)
+				converted := presenters.PresentCloudRegion(&cloudRegion)
+				regionList.Items = append(regionList.Items, converted)
+			}
+			return regionList, nil
+		},
+		ErrorHandler: handleError,
+	}
+	handleGet(w, r, cfg)
+}
+
+func (h cloudProvidersHandler) ListCloudProviders(w http.ResponseWriter, r *http.Request) {
+
+	cfg := &handlerConfig{
+		Action: func() (i interface{}, serviceError *errors.ServiceError) {
+
+			cloudProviders, err := h.service.ListCloudProviders()
+			if err != nil {
+				return nil, err
+			}
+			cloudProviderList := openapi.CloudProviderList{
+				Kind:  "CloudProviderList",
+				Total: int32(len(cloudProviders)),
+				Size:  int32(len(cloudProviders)),
+				Page:  int32(1),
+			}
+
+			for _, cloudProvider := range cloudProviders {
+				cloudProvider.Enabled = h.config.IsProviderSupported(cloudProvider.Id)
+				converted := presenters.PresentCloudProvider(&cloudProvider)
+				cloudProviderList.Items = append(cloudProviderList.Items, converted)
+			}
+
+			return cloudProviderList, nil
+		},
+		ErrorHandler: handleError,
+	}
+	handleGet(w, r, cfg)
+}
