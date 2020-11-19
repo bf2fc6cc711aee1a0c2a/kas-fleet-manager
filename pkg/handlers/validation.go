c3b2ea17cdc1dbe6442fa92e655b4d68f84df1ec
Manyanda Chitimbo <mchitimb@redhat.com>
2020-11-19 09:56:20 +0100

    fix(kafka): pick right default region according to provider
    Choose the correct default region according to the provided or selected provider instead of always
    using the default region from the default cloud provider. Also lazily find the default provider
    instead of doing it systematic
    
    To verify:
    - before this patch, if you had the following content in the provider
    configuration file.
    
    ```yaml
    ---
    supported_providers:
      - name: aws
        default: true
        regions:
          - name: us-east-1
            default: true
      - name: gcp
        default: false
        regions:
          - name: us-west-2
            default: true
    ```
    
    and you run the API,
    
    sending the following request will fail:
    ```bash
    curl -v -XPOST -H "Authorization: Bearer $(ocm token)" http://localhost:8000/api/managed-services-api/v1/kafkas\?async\=true -d '{ "cloud_provider": "gcp",  "name": "serviceapi", "multi_az": true}'
    ```
    
    with
    ```json
    {"id":"8","kind":"Error","href":"/api/managed-services-api/v1/errors/8","code":"MGD-SERV-API-8","reason":"region us-east-1 is not supported for gcp, supported regions are: [us-west-2]","operation_id":"1kVBY67CcqwgeaRdsa0nmCsbWYJ"}
    ```
    
    I noticed it when looking at
    https://issues.redhat.com/browse/MGDSTRM-512 and the corresponding PR to
    get inspiration for https://issues.redhat.com/browse/MGDSTRM-659

--- pkg/handlers/validation.go
+++ pkg/handlers/validation.go
@@ -2,10 +2,12 @@ package handlers
 
 import (
 	"fmt"
-	"gitlab.cee.redhat.com/service/managed-services-api/pkg/api/openapi"
-	"gitlab.cee.redhat.com/service/managed-services-api/pkg/services"
 	"net/http"
 	"strings"
+
+	"gitlab.cee.redhat.com/service/managed-services-api/pkg/api/openapi"
+	"gitlab.cee.redhat.com/service/managed-services-api/pkg/config"
+	"gitlab.cee.redhat.com/service/managed-services-api/pkg/services"
 
 	"gitlab.cee.redhat.com/service/managed-services-api/pkg/errors"
 )
@@ -71,33 +73,35 @@ func validateMultiAZEnabled(value *bool, action string) validate {
 
 // validateCloudProvider returns a validator that sets default cloud provider details if needed and validates provided
 // provider and region
-func validateCloudProvider(kr *openapi.KafkaRequest, configService services.ConfigService, action string) validate {
+func validateCloudProvider(kafkaRequest *openapi.KafkaRequest, configService services.ConfigService, action string) validate {
 	return func() *errors.ServiceError {
-		defaultProvider, err := configService.GetDefaultProvider()
-		if err != nil {
-			return errors.GeneralError("failed to get default provider configuration")
-		}
-
-		if kr.CloudProvider == "" {
-			kr.CloudProvider = defaultProvider.Name
-		}
-		if kr.Region == "" {
-			defaultRegion, err := configService.GetDefaultRegionForProvider(defaultProvider)
+		var provider config.Provider
+		if kafkaRequest.CloudProvider == "" {
+			var err error
+			provider, err = configService.GetDefaultProvider()
 			if err != nil {
-				return errors.GeneralError(fmt.Sprintf("failed to get default region for provider %s", defaultProvider.Name))
+				return errors.GeneralError("failed to get default provider configuration")
 			}
-			kr.Region = defaultRegion.Name
+			kafkaRequest.CloudProvider = provider.Name
+		} else {
+			var providerSupported bool
+			provider, providerSupported = configService.GetSupportedProviders().GetByName(kafkaRequest.CloudProvider)
+			if !providerSupported {
+				return errors.Validation("provider %s is not supported, supported providers are: %s", kafkaRequest.CloudProvider, configService.GetSupportedProviders())
+			}
 		}
 
-		providerSupported := configService.IsProviderSupported(kr.CloudProvider)
-		if !providerSupported {
-			return errors.Validation("provider %s is not supported, supported providers are: %s", kr.CloudProvider, configService.GetSupportedProviders())
+		if kafkaRequest.Region == "" {
+			defaultRegionForProvider, err := configService.GetDefaultRegionForProvider(provider)
+			if err != nil {
+				return errors.GeneralError(fmt.Sprintf("failed to get default region for provider %s", provider.Name))
+			}
+			kafkaRequest.Region = defaultRegionForProvider.Name
 		}
 
-		regionSupported := configService.IsRegionSupportedForProvider(kr.CloudProvider, kr.Region)
+		regionSupported := configService.IsRegionSupportedForProvider(kafkaRequest.CloudProvider, kafkaRequest.Region)
 		if !regionSupported {
-			provider, _ := configService.GetSupportedProviders().GetByName(kr.CloudProvider)
-			return errors.Validation("region %s is not supported for %s, supported regions are: %s", kr.Region, kr.CloudProvider, provider.Regions)
+			return errors.Validation("region %s is not supported for %s, supported regions are: %s", kafkaRequest.Region, kafkaRequest.CloudProvider, provider.Regions)
 		}
 
 		return nil
