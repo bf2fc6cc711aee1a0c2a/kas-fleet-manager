401ce09c0cc8dcddc6e1ae670d2027cf5ca09068
Pawel Paszki <pawelpaszki@gmail.com>
2020-11-25 11:04:25 +0000

    refactored make targets to return errors on test failure but keep going with cluster cleanup

--- pr_check_docker.sh
+++ pr_check_docker.sh
@@ -29,8 +29,8 @@ which pg_ctl
 PGDATA=/var/lib/postgresql/data /usr/lib/postgresql/*/bin/pg_ctl -w stop
 PGDATA=/var/lib/postgresql/data /usr/lib/postgresql/*/bin/pg_ctl start -o "-c listen_addresses='*' -p 5432"
 
-# run the make commands
-make verify test test-integration
+# check the code. Then run the unit and integration tests and cleanup cluster (if running against real OCM)
+make -k verify test test-integration cluster/cleanup
 
 # required for entrypoint script run by docker to exit and stop container
 exit 0
