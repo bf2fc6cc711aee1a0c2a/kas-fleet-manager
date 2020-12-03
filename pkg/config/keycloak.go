8d8ff7a1df8c0b9fd959ded4a5acdbf29b3da4ba
Abhishek Koserwal <akoserwa@redhat.com>
2020-12-03 11:01:33 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- pkg/config/keycloak.go
+++ pkg/config/keycloak.go
@@ -51,7 +51,7 @@ func (kc *KeycloakConfig) AddFlags(fs *pflag.FlagSet) {
 	fs.StringVar(&kc.BaseURL, "mas-sso-base-url", kc.BaseURL, "The base URL of the mas-sso, integration by default")
 	fs.StringVar(&kc.BaseUrlFile, "mas-sso-base-url-file", kc.BaseURL, "File containing base URL of the MAS Keycloak")
 	fs.StringVar(&kc.Realm, "mas-sso-realm", kc.Realm, "Realm for the mas-sso")
-	fs.StringVar(&kc.RealmFile, "mas-sso-realm-file", kc.RealmFile, "File contains Realm for the mas-sso")
+	fs.StringVar(&kc.RealmFile, "mas-sso-realm-file", kc.RealmFile, "File containing Realm for the mas-sso")
 	fs.StringVar(&kc.TLSTrustedCertificatesFile, "mas-sso-cert-file", kc.TLSTrustedCertificatesFile, "File contains tls cert for the mas-sso")
 	fs.BoolVar(&kc.Debug, "mas-sso-debug", kc.Debug, "Debug flag for Keycloak API")
 	fs.BoolVar(&kc.InsecureSkipVerify, "mas-sso-insecure", kc.InsecureSkipVerify, "disable tls verification with mas-sso")
