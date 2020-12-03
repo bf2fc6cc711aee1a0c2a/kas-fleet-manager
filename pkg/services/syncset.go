5c3fb1b2d34979d11cadf302a23f8177ccb01e2f
akoserwa <akoserwa@redhat.com>
2020-12-03 20:07:49 +0530

    removing the comment for clientid

--- pkg/services/syncset.go
+++ pkg/services/syncset.go
@@ -359,7 +359,6 @@ func newKafkaSyncsetBuilder(kafkaRequest *api.KafkaRequest, keycloakConfig *conf
 		}
 		brokerOverrides = append(brokerOverrides, brokerOverride)
 	}
-	//ssoClientID := buildKeycloakClientNameIdentifier(kafkaRequest)
 
 	// Derive Kafka config based on global constants
 	kafkaConfig := map[string]string{
