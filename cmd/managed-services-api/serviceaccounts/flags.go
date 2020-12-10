e6d099509d2c34af4cb99a35b48d4bf0be234286
Abhishek Koserwal <akoserwa@redhat.com>
2020-12-10 12:49:36 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- cmd/managed-services-api/serviceaccounts/flags.go
+++ cmd/managed-services-api/serviceaccounts/flags.go
@@ -1,7 +1,7 @@
 package serviceaccounts
 
 const (
-	// FlagID is a flag representing a service account id
+	// FlagSaID is a flag representing a service account id
 	FlagSaID = "id"
 	// FlagName is a flag representing a service account name
 	FlagName = "name"
