f2d16480c3887c6cb8d6bcc701b473a9afb48d1b
Manyanda Chitimbo <mchitimb@redhat.com>
2021-01-19 12:15:40 +0100

    feat(cluster-creation): externalise openshift version
    also make it possible to not specifying the version thus defaulting to latest stable
    
    Closes https://issues.redhat.com/browse/MGDSTRM-904

--- pkg/config/cluster_creation.go
+++ pkg/config/cluster_creation.go
@@ -13,13 +13,13 @@ type ClusterCreationConfig struct {
 func NewClusterCreationConfig() *ClusterCreationConfig {
 	return &ClusterCreationConfig{
 		AutoOSDCreation:    false,
-		OpenshiftVersion:   "openshift-v4.6.1",
+		OpenshiftVersion:   "",
 		ComputeMachineType: "m5.4xlarge",
 	}
 }
 
 func (s *ClusterCreationConfig) AddFlags(fs *pflag.FlagSet) {
 	fs.BoolVar(&s.AutoOSDCreation, "auto-osd-creation", s.AutoOSDCreation, "Enable Auto Creation of supported OSD cluster")
-	fs.StringVar(&s.OpenshiftVersion, "cluster-openshift-version", s.OpenshiftVersion, "The version of openshift installed on the cluster")
+	fs.StringVar(&s.OpenshiftVersion, "cluster-openshift-version", s.OpenshiftVersion, "The version of openshift installed on the cluster. An empty string indicates that the latest stable version should be used")
 	fs.StringVar(&s.ComputeMachineType, "cluster-compute-machine-type", s.ComputeMachineType, "The compute machine type")
 }
