e595a5e0283575145d2b37fc677b429d9bb1dc3f
Wei Li <wei.li@redhat.com>
2021-02-03 17:30:29 +0000

    feat: MGDSTRM-1323 initial support for enabling agent operators

--- pkg/config/cluster_creation.go
+++ pkg/config/cluster_creation.go
@@ -5,16 +5,18 @@ import (
 )
 
 type ClusterCreationConfig struct {
-	AutoOSDCreation    bool   `json:"auto_osd_creation"`
-	OpenshiftVersion   string `json:"cluster_openshift_version"`
-	ComputeMachineType string `json:"cluster_compute_machine_type"`
+	AutoOSDCreation             bool   `json:"auto_osd_creation"`
+	OpenshiftVersion            string `json:"cluster_openshift_version"`
+	ComputeMachineType          string `json:"cluster_compute_machine_type"`
+	EnableKasFleetshardOperator bool   `json:"enable_kas_fleetshard_operator"`
 }
 
 func NewClusterCreationConfig() *ClusterCreationConfig {
 	return &ClusterCreationConfig{
-		AutoOSDCreation:    false,
-		OpenshiftVersion:   "",
-		ComputeMachineType: "m5.4xlarge",
+		AutoOSDCreation:             false,
+		OpenshiftVersion:            "",
+		ComputeMachineType:          "m5.4xlarge",
+		EnableKasFleetshardOperator: false,
 	}
 }
 
@@ -22,4 +24,5 @@ func (s *ClusterCreationConfig) AddFlags(fs *pflag.FlagSet) {
 	fs.BoolVar(&s.AutoOSDCreation, "auto-osd-creation", s.AutoOSDCreation, "Enable Auto Creation of supported OSD cluster")
 	fs.StringVar(&s.OpenshiftVersion, "cluster-openshift-version", s.OpenshiftVersion, "The version of openshift installed on the cluster. An empty string indicates that the latest stable version should be used")
 	fs.StringVar(&s.ComputeMachineType, "cluster-compute-machine-type", s.ComputeMachineType, "The compute machine type")
+	fs.BoolVar(&s.EnableKasFleetshardOperator, "enable-kas-fleetshard-operator", s.EnableKasFleetshardOperator, "Enable kas-fleetshard-operator when creating an OSD cluster")
 }
