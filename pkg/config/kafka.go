751515b2b9208fa32dc1040b8a839adae1edb180
Massimiliano Ziccardi <mziccard@redhat.com>
2021-02-10 16:59:34 +0100

    Adds ManagedKafkaCR object

--- pkg/config/kafka.go
+++ pkg/config/kafka.go
@@ -14,6 +14,7 @@ type KafkaConfig struct {
 	KafkaDomainName                string `json:"kafka_domain_name"`
 	KafkaCanaryImage               string `json:"kafka_canary_image"`
 	KafkaAdminServerImage          string `json:"kafka_admin_server_image"`
+	EnableManagedKafkaCR           bool   `json:"enable_managedkafka_cr"`
 }
 
 func NewKafkaConfig() *KafkaConfig {
@@ -34,6 +35,7 @@ func (c *KafkaConfig) AddFlags(fs *pflag.FlagSet) {
 	fs.BoolVar(&c.EnableKafkaExternalCertificate, "enable-kafka-external-certificate", c.EnableKafkaExternalCertificate, "Enable custom certificate for Kafka TLS")
 	fs.StringVar(&c.KafkaCanaryImage, "kafka-canary-image", c.KafkaCanaryImage, "Specifies a canary image")
 	fs.StringVar(&c.KafkaAdminServerImage, "kafka-admin-server-image", c.KafkaAdminServerImage, "Specifies an admin server image")
+	fs.BoolVar(&c.EnableManagedKafkaCR, "enable-managed-kafka-cr", c.EnableManagedKafkaCR, "Enable usage of the ManagedKafkaCR instead of the KafkaCR")
 }
 
 func (c *KafkaConfig) ReadFiles() error {
