375c4641b15c869a1d33e0907358104420392090
Miguel Soriano <msoriano@redhat.com>
2021-02-25 22:08:18 +0100

    Set some DataPlaneClusterUpdateStatusRequest OpenAPI type fields as a pointer
    To be able to differentiate between unset and set with a zero-value

--- pkg/api/presenters/data_plane_cluster_status.go
+++ pkg/api/presenters/data_plane_cluster_status.go
@@ -18,25 +18,25 @@ func ConvertDataPlaneClusterStatus(status openapi.DataPlaneClusterUpdateStatusRe
 	return &api.DataPlaneClusterStatus{
 		Conditions: conds,
 		NodeInfo: api.DataPlaneClusterStatusNodeInfo{
-			Ceiling:                int(status.NodeInfo.Ceiling),
-			Floor:                  int(status.NodeInfo.Floor),
-			Current:                int(status.NodeInfo.Current),
-			CurrentWorkLoadMinimum: int(status.NodeInfo.CurrentWorkLoadMinimum),
+			Ceiling:                int(*status.NodeInfo.Ceiling),
+			Floor:                  int(*status.NodeInfo.Floor),
+			Current:                int(*status.NodeInfo.Current),
+			CurrentWorkLoadMinimum: int(*status.NodeInfo.CurrentWorkLoadMinimum),
 		},
 		ResizeInfo: api.DataPlaneClusterStatusResizeInfo{
-			NodeDelta: int(status.ResizeInfo.NodeDelta),
+			NodeDelta: int(*status.ResizeInfo.NodeDelta),
 			Delta: api.DataPlaneClusterStatusCapacity{
-				IngressEgressThroughputPerSec: status.ResizeInfo.Delta.IngressEgressThroughputPerSec,
-				Connections:                   int(status.ResizeInfo.Delta.Connections),
-				DataRetentionSize:             status.ResizeInfo.Delta.DataRetentionSize,
-				Partitions:                    int(status.ResizeInfo.Delta.MaxPartitions),
+				IngressEgressThroughputPerSec: *status.ResizeInfo.Delta.IngressEgressThroughputPerSec,
+				Connections:                   int(*status.ResizeInfo.Delta.Connections),
+				DataRetentionSize:             *status.ResizeInfo.Delta.DataRetentionSize,
+				Partitions:                    int(*status.ResizeInfo.Delta.MaxPartitions),
 			},
 		},
 		Remaining: api.DataPlaneClusterStatusCapacity{
-			IngressEgressThroughputPerSec: status.Remaining.IngressEgressThroughputPerSec,
-			Connections:                   int(status.Remaining.Connections),
-			DataRetentionSize:             status.Remaining.DataRetentionSize,
-			Partitions:                    int(status.Remaining.Partitions),
+			IngressEgressThroughputPerSec: *status.Remaining.IngressEgressThroughputPerSec,
+			Connections:                   int(*status.Remaining.Connections),
+			DataRetentionSize:             *status.Remaining.DataRetentionSize,
+			Partitions:                    int(*status.Remaining.Partitions),
 		},
 	}
 }
--- pkg/api/private/openapi/docs/DataPlaneClusterUpdateStatusRequestTotal.md
+++ pkg/api/private/openapi/docs/DataPlaneClusterUpdateStatusRequestTotal.md
@@ -4,10 +4,10 @@
 
 Name | Type | Description | Notes
 ------------ | ------------- | ------------- | -------------
-**IngressEgressThroughputPerSec** | **string** |  | [optional] 
-**Connections** | **int32** |  | [optional] 
-**DataRetentionSize** | **string** |  | [optional] 
-**Partitions** | **int32** |  | [optional] 
+**IngressEgressThroughputPerSec** | Pointer to **string** |  | [optional] 
+**Connections** | Pointer to **int32** |  | [optional] 
+**DataRetentionSize** | Pointer to **string** |  | [optional] 
+**Partitions** | Pointer to **int32** |  | [optional] 
 
 [[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
 
