2f2e6ad7902e9b9af854013d7b0d99a3685d9fa8
Pawel Paszki <pawelpaszki@gmail.com>
2021-03-02 20:00:27 +0000

    Add flexibility over what endpoints to be attacked in performance testing (#30)
    * added GET only param for performance tests and changed load distribution
    
    * completely disabled DELETE when running GET requests
    
    * updated readme

--- test/performance/Makefile.mk
+++ test/performance/Makefile.mk
@@ -3,11 +3,15 @@ PERF_TEST_USERS ?= 150 # number of locust test users - more users - more load ca
 PERF_TEST_USER_SPAWN_RATE ?= 1 # frequency of user spawning (per second)
 PERF_TEST_RUN_TIME ?= 120m # running time (in minutes - as our locustfile expects minutes)
 PERF_TEST_WORKERS_NUMBER ?= 25 # number of running worker containers - more containers - more load can be sent
-PERF_TEST_PREPOPULATE_DB ?= FALSE # whether to prepopulate db with kafka_requests (must be FALSE for staging/producton cluster)
+PERF_TEST_PREPOPULATE_DB ?= FALSE # whether to prepopulate db with kafka_requests (MUST BE FALSE for staging/producton cluster)
 # number of kafkas to prepopulate (in deleted state) per worker (only if PERF_TEST_PREPOPULATE_DB == TRUE)
 PERF_TEST_PREPOPULATE_DB_KAFKA_PER_WORKER ?= 500
 # number of kafkas to create during performance testing of the API per locust worker
-PERF_TEST_KAFKAS_PER_WORKER ?= 5
+PERF_TEST_KAFKAS_PER_WORKER ?= 0
+# by default only GET endpoints will be attacked
+PERF_TEST_GET_ONLY ?= TRUE
+# wait time before creating another kafka by a worker (in seconds)
+PERF_TEST_KAFKA_POST_WAIT_TIME ?= 1
 
 # run performance tests
 # requires at least the api route to be set as "PERF_TEST_ROUTE_HOST" and "OCM_OFFLINE_TOKEN"
@@ -20,7 +24,8 @@ else
 	
 	PERF_TEST_ROUTE_HOST=$(PERF_TEST_ROUTE_HOST) PERF_TEST_USERS=$(PERF_TEST_USERS) PERF_TEST_USER_SPAWN_RATE=$(PERF_TEST_USER_SPAWN_RATE) \
 		PERF_TEST_RUN_TIME=$(PERF_TEST_RUN_TIME) PERF_TEST_PREPOPULATE_DB=$(PERF_TEST_PREPOPULATE_DB) PERF_TEST_KAFKAS_PER_WORKER=$(PERF_TEST_KAFKAS_PER_WORKER) \
-			PERF_TEST_PREPOPULATE_DB_KAFKA_PER_WORKER=$(PERF_TEST_PREPOPULATE_DB_KAFKA_PER_WORKER) \
-				docker-compose --file test/performance/docker-compose.yml up --scale secondary=$(PERF_TEST_WORKERS_NUMBER) --remove-orphans
+			PERF_TEST_PREPOPULATE_DB_KAFKA_PER_WORKER=$(PERF_TEST_PREPOPULATE_DB_KAFKA_PER_WORKER) PERF_TEST_GET_ONLY=$(PERF_TEST_GET_ONLY) \
+				PERF_TEST_KAFKA_POST_WAIT_TIME=$(PERF_TEST_KAFKA_POST_WAIT_TIME) \
+			  	docker-compose --file test/performance/docker-compose.yml up --scale secondary=$(PERF_TEST_WORKERS_NUMBER) --remove-orphans
 endif
 .PHONY: test/performance
