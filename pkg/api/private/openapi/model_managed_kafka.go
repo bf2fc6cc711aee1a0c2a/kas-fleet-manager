2166cdcf52819cbeb73520b6c8e16b4380095805
Wei Li <wei.li@redhat.com>
2021-03-05 15:56:51 +0000

    fix: make sure ManagedKafka CR is generated in the correct format as expected by the kas-fleetshard operator

--- pkg/api/private/openapi/model_managed_kafka.go
+++ pkg/api/private/openapi/model_managed_kafka.go
@@ -11,11 +11,10 @@ package openapi
 
 // ManagedKafka struct for ManagedKafka
 type ManagedKafka struct {
-	Id         string                      `json:"id,omitempty"`
-	Kind       string                      `json:"kind,omitempty"`
-	Href       string                      `json:"href,omitempty"`
-	Name       string                      `json:"name,omitempty"`
-	Annotation ManagedKafkaAllOfAnnotation `json:"annotation,omitempty"`
-	Spec       ManagedKafkaAllOfSpec       `json:"spec,omitempty"`
-	Status     ManagedKafkaAllOfStatus     `json:"status,omitempty"`
+	Id       string                    `json:"id,omitempty"`
+	Kind     string                    `json:"kind,omitempty"`
+	Href     string                    `json:"href,omitempty"`
+	Metadata ManagedKafkaAllOfMetadata `json:"metadata,omitempty"`
+	Spec     ManagedKafkaAllOfSpec     `json:"spec,omitempty"`
+	Status   ManagedKafkaAllOfStatus   `json:"status,omitempty"`
 }
