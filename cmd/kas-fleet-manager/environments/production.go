6239cf90886fe93f7a2864f1250a98f5cc6440f5
Manyanda Chitimbo <mchitimb@redhat.com>
2021-04-01 11:57:17 +0200

    fix: remove flag

--- cmd/kas-fleet-manager/environments/production.go
+++ cmd/kas-fleet-manager/environments/production.go
@@ -12,7 +12,6 @@ var productionConfigDefaults map[string]string = map[string]string{
 	"enable-allow-list":                 "true",
 	"enable-deny-list":                  "true",
 	"max-allowed-instances":             "1",
-	"auto-osd-creation":                 "true",
 	"mas-sso-realm":                     "mas-sso",
 	"mas-sso-base-url":                  "https://keycloak-edge-redhat-rhoam-user-sso.apps.mas-sso-stage.1gzl.s1.devshift.org",
 	"enable-kafka-external-certificate": "true",
