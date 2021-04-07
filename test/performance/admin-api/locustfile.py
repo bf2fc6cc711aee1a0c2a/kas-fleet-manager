98b8bec4b104f38fe91ac3f6c9cd79d42c7e940b
Pawel Paszki <pawelpaszki@gmail.com>
2021-04-07 13:50:52 +0100

    addressed PR comment

--- test/performance/admin-api/locustfile.py
+++ test/performance/admin-api/locustfile.py
@@ -11,6 +11,7 @@ if str(sso_url) == 'None' or str(svc_acc_client_id) == 'None' or str(svc_acc_cli
   sys.exit('Some of required params not specified (ADMIN_API_SSO_AUTH_URL or ADMIN_API_SVC_ACC_ID or ADMIN_API_SVC_ACC_SECRET)')
 
 topic_name = 'topic01'
+post_request_timeout = 0.65 # seconds
 
 class MyUser(HttpUser):
 
@@ -22,15 +23,15 @@ class MyUser(HttpUser):
       json_response = json.loads(r.text)
       self.client.headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + json_response['access_token']}
       with self.client.get('/rest/topics', verify=False) as response:
-        time.sleep(0.65)
+        time.sleep(post_request_timeout)
         json_data = {'name': topic_name, 'settings': { 'numPartitions': 3, 'config': [{'key': 'cleanup.policy','value': 'delete'}]}}
         with self.client.post('/rest/topics', json=json_data, verify=False, catch_response=True ) as response:
           if response.status_code == 201 or response.status_code == 409:
-            time.sleep(0.65)
+            time.sleep(post_request_timeout)
             self.client.get(f'/rest/topics/{topic_name}', verify=False)
-            time.sleep(0.65)
+            time.sleep(post_request_timeout)
             delete_status = 0
             while delete_status != 200:
               with self.client.delete(f'/rest/topics/{topic_name}', verify=False, catch_response=True) as response:
-                time.sleep(0.65)
+                time.sleep(post_request_timeout)
                 delete_status = response.status_code
