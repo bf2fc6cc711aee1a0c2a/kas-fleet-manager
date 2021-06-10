e882a4abc91365fa13895c5331697ef2ea6a6243
Wei Li <wei.li@redhat.com>
2021-06-10 16:57:43 +0100

    ci: publish the image to the bf2 org (#373)

--- build_deploy.sh
+++ build_deploy.sh
@@ -31,6 +31,11 @@
 # The version should be the short hash from git. This is what the deployent process expects.
 VERSION="$(git log --pretty=format:'%h' -n 1)"
 
+# Set the variable required to login and push images to the registry
+QUAY_USER=${QUAY_USER_NAME:-$RHOAS_QUAY_USER}
+QUAY_TOKEN=${QUAY_USER_PASSWORD:-$RHOAS_QUAY_TOKEN}
+QUAY_ORG=${QUAY_ORG_NAME:-rhoas}
+
 # Set the directory for docker configuration:
 DOCKER_CONFIG="${PWD}/.docker"
 
@@ -49,41 +54,49 @@ cd "${LINK}"
 # Log in to the image registry:
 if [ -z "${QUAY_USER}" ]; then
   echo "The quay.io push user name hasn't been provided."
-  echo "Make sure to set the QUAY_USER environment variable."
+  echo "Make sure to set the QUAY_USER_NAME environment variable."
   exit 1
 fi
 if [ -z "${QUAY_TOKEN}" ]; then
   echo "The quay.io push token hasn't been provided."
-  echo "Make sure to set the QUAY_TOKEN environment variable."
+  echo "Make sure to set the QUAY_USER_PASSWORD environment variable."
   exit 1
 fi
 
 # Set up the docker config directory
 mkdir -p "${DOCKER_CONFIG}"
 
-# Push the image:
-if [[ ! -z "${RHOAS_QUAY_USER}" ]] && [[ ! -z "${RHOAS_QUAY_TOKEN}" ]]; then
-  echo "RHOAS Quay.io user and token is set, will push images to RHOAS org"
-  make \
-    DOCKER_CONFIG="${DOCKER_CONFIG}" \
-    QUAY_USER="${RHOAS_QUAY_USER}" \
-    QUAY_TOKEN="${RHOAS_QUAY_TOKEN}" \
-    version="${VERSION}" \
-    external_image_registry="quay.io" \
-    internal_image_registry="quay.io" \
-    image_repository="rhoas/kas-fleet-manager" \
-    docker/login \
-    image/push
-
-  BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-  make \
-    DOCKER_CONFIG="${DOCKER_CONFIG}" \
-    QUAY_USER="${RHOAS_QUAY_USER}" \
-    QUAY_TOKEN="${RHOAS_QUAY_TOKEN}" \
-    version="${BRANCH}" \
-    external_image_registry="quay.io" \
-    internal_image_registry="quay.io" \
-    image_repository="rhoas/kas-fleet-manager" \
-    docker/login \
-    image/push
+BRANCH="main"
+if [[ ! -z "$GITHUB_REF" ]]; then
+  BRANCH="$(echo $GITHUB_REF | awk -F/ '{print $NF}')"
+  echo "GITHUB_REF is defined. Set image tag to $BRANCH."
+elif [[ ! -z "$GIT_BRANCH" ]]; then
+  BRANCH="$(echo $GIT_BRANCH | awk -F/ '{print $NF}')"
+  echo "GIT_BRANCH is defined. Set image tag to $BRANCH."
+else
+  echo "No git branch env var found. Set image tag to $BRANCH."
 fi
+
+# Push the image:
+echo "Quay.io user and token is set, will push images to $QUAY_ORG org"
+make \
+  DOCKER_CONFIG="${DOCKER_CONFIG}" \
+  QUAY_USER="${QUAY_USER}" \
+  QUAY_TOKEN="${QUAY_TOKEN}" \
+  version="${VERSION}" \
+  external_image_registry="quay.io" \
+  internal_image_registry="quay.io" \
+  image_repository="$QUAY_ORG/kas-fleet-manager" \
+  docker/login \
+  image/push
+
+make \
+  DOCKER_CONFIG="${DOCKER_CONFIG}" \
+  QUAY_USER="${QUAY_USER}" \
+  QUAY_TOKEN="${QUAY_TOKEN}" \
+  version="${BRANCH}" \
+  external_image_registry="quay.io" \
+  internal_image_registry="quay.io" \
+  image_repository="$QUAY_ORG/kas-fleet-manager" \
+  docker/login \
+  image/push
