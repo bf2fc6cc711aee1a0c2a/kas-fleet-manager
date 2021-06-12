6dba3378d5ca8159e96f19cf7c6c44fbb7990c82
Hiram Chirino <hiram@hiramchirino.com>
2021-06-12 13:23:17 -0400

    refactor: create ocm and observatorium clients using di, allows to initialize with the services.

--- test/registration.go
+++ test/registration.go
@@ -41,9 +41,6 @@ func RegisterIntegrationWithHooks(t *testing.T, server *httptest.Server, startHo
 	// Reload the clients and services to ensure the following:
 	//   - Services should use the mocked observatorium client
 	//   - Apply any configuration changes that could change the way clients/services are loaded (i.e. cluster placement strategy service)
-	if err := helper.Env().LoadClients(); err != nil {
-		t.Fatal("failed to reload clients")
-	}
 	if err := helper.Env().LoadServices(); err != nil {
 		t.Fatal("failed to reload services")
 	}
