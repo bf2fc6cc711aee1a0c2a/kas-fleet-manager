34a144830f0f1fbbe717e903a80823ee12c55085
Hiram Chirino <hiram@hiramchirino.com>
2021-06-21 11:46:42 -0400

    fix: don't load config for all services in the migrate case so that we don't need to fully configure it's kube container env.

--- internal/kafka/internal/cmd/kafka/cmd.go
+++ internal/kafka/internal/cmd/kafka/cmd.go
@@ -4,6 +4,7 @@ package kafka
 
 import (
 	"github.com/bf2fc6cc711aee1a0c2a/kas-fleet-manager/pkg/environments"
+	"github.com/golang/glog"
 	"github.com/spf13/cobra"
 )
 
@@ -12,6 +13,12 @@ func NewKafkaCommand(env *environments.Env) *cobra.Command {
 		Use:   "kafka",
 		Short: "Perform kafka CRUD actions directly",
 		Long:  "Perform kafka CRUD actions directly.",
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			err := env.LoadConfigAndCreateServices()
+			if err != nil {
+				glog.Fatalf("Unable to initialize environment: %s", err.Error())
+			}
+		},
 	}
 
 	// add sub-commands
