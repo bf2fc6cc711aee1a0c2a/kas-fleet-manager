f3e305f87e7393442f6d19387beabea194b2daae
Manyanda Chitimbo <mchitimb@redhat.com>
2021-07-01 15:41:19 +0200

    feat(scripts): dump file and log an error when cluster id not found

--- scripts/cleanup_test_cluster.sh
+++ scripts/cleanup_test_cluster.sh
@@ -8,11 +8,18 @@ INTEGRATION_ENV="integration"
 ENV="$OCM_ENV"
 if test -f "$FILE"; then
   if [[ $ENV != "$INTEGRATION_ENV" ]] ; then
+    # Dump file to be able to inspect content during debugging
+    echo "Dumping content of $FILE:"
+    cat $FILE
+    # Retrieve cluster id
     clusterID=$(jq -r .cluster_id ${FILE})
     if [[ $clusterID != "null" ]] ; then
       ocm login --url=https://api.stage.openshift.com/ --token="$OCM_OFFLINE_TOKEN"
       ocm delete /api/clusters_mgmt/v1/clusters/"$clusterID"
       rm $FILE
+    else 
+      echo "Cluster id not found in file $FILE"
+      exit 1  
     fi
   fi
 else 
