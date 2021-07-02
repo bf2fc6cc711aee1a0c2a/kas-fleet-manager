b9b9dc3a3a4a6fe4060659af8297bdd0a7b11da2
Wei Li <wei.li@redhat.com>
2021-07-02 12:29:34 +0100

    feat(auth): add auth/authz middleware for the admin endpoint

--- pkg/auth/roles_authz_middleware.go
+++ pkg/auth/roles_authz_middleware.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"github.com/golang/glog"
 	"net/http"
 	"strings"
 
@@ -9,10 +10,18 @@ import (
 	"github.com/dgrijalva/jwt-go"
 )
 
+const (
+	KasFleetManagerAdminReadRole  = "kas-fleet-manager-admin-read"
+	KasFleetManagerAdminWriteRole = "kas-fleet-manager-admin-write"
+	KasFleetManagerAdminFullRole  = "kas-fleet-manager-admin-full"
+)
+
 // RolesAuthorizationMiddleware can be used to perform RBAC authorization checks on endpoints
 type RolesAuthorizationMiddleware interface {
 	// RequireRealmRole will check the given realm role exists in the request token
 	RequireRealmRole(roleName string, code errors.ServiceErrorCode) func(handler http.Handler) http.Handler
+	// RequireRolesForMethods will check that at least one of the realm roles exists in the request token based on the http method in the request
+	RequireRolesForMethods(roles map[string][]string, code errors.ServiceErrorCode) func(handler http.Handler) http.Handler
 }
 
 type rolesAuthMiddleware struct{}
@@ -40,6 +49,39 @@ func (m *rolesAuthMiddleware) RequireRealmRole(roleName string, code errors.Serv
 				shared.HandleError(request, writer, serviceErr)
 				return
 			}
+		})
+	}
+}
+
+func (m *rolesAuthMiddleware) RequireRolesForMethods(roles map[string][]string, code errors.ServiceErrorCode) func(handler http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+			serviceErr := errors.New(code, "")
+			method := request.Method
+			allowedRoles, ok := roles[method]
+			if !ok {
+				// no allowed roles defined for the given method, deny the request by default to be safer
+				glog.Infof("no allowed roles defined for method %s, deny the request for url %s", method, request.URL)
+				shared.HandleError(request, writer, serviceErr)
+				return
+			}
+
+			ctx := request.Context()
+			claims, err := GetClaimsFromContext(ctx)
+			if err != nil {
+				shared.HandleError(request, writer, serviceErr)
+				return
+			}
+			realmRoles := getRealmRolesClaim(claims)
+			// if the request claim has any realm role that is defined in the `roles` map, the request will be allowed
+			for _, r := range allowedRoles {
+				if hasRole(realmRoles, r) {
+					next.ServeHTTP(writer, request)
+					return
+				}
+			}
+			// no matching roles found, deny the request
+			shared.HandleError(request, writer, serviceErr)
 		})
 	}
 }
