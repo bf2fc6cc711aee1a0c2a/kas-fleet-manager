e03418d161766fb87522a069b9bc7d2f3253cff0
Manyanda Chitimbo <mchitimb@redhat.com>
2021-08-05 14:10:10 +0200

    refactor(authz): remove authorization middleware
    The middleware was not used and can be safely removed. See
    https://issues.redhat.com/browse/MGDSTRM-4679 for details

--- pkg/services/authorization/authorization.go
+++ pkg/services/authorization/authorization.go
@@ -1,18 +1,13 @@
 package authorization
 
 import (
-	"context"
 	"fmt"
 	"net/http"
 
 	sdkClient "github.com/openshift-online/ocm-sdk-go"
-
-	azv1 "github.com/openshift-online/ocm-sdk-go/authorizations/v1"
 )
 
 type Authorization interface {
-	SelfAccessReview(ctx context.Context, action, resourceType, organizationID, subscriptionID, clusterID string) (allowed bool, err error)
-	AccessReview(ctx context.Context, username, action, resourceType, organizationID, subscriptionID, clusterID string) (allowed bool, err error)
 	CheckUserValid(username string, orgId string) (bool, error)
 }
 
@@ -26,66 +21,6 @@ func NewOCMAuthorization(client *sdkClient.Connection) Authorization {
 	return &authorization{
 		client: client,
 	}
-}
-
-func (a authorization) SelfAccessReview(ctx context.Context, action, resourceType, organizationID, subscriptionID, clusterID string) (allowed bool, err error) {
-	con := a.client
-	selfAccessReview := con.Authorizations().V1().SelfAccessReview()
-
-	request, err := azv1.NewSelfAccessReviewRequest().
-		Action(action).
-		ResourceType(resourceType).
-		OrganizationID(organizationID).
-		ClusterID(clusterID).
-		SubscriptionID(subscriptionID).
-		Build()
-	if err != nil {
-		return false, err
-	}
-
-	postResp, err := selfAccessReview.Post().
-		Request(request).
-		SendContext(ctx)
-	if err != nil {
-		return false, err
-	}
-	response, ok := postResp.GetResponse()
-	if !ok {
-		return false, fmt.Errorf("Empty response from authorization post request")
-	}
-
-	return response.Allowed(), nil
-}
-
-func (a authorization) AccessReview(ctx context.Context, username, action, resourceType, organizationID, subscriptionID, clusterID string) (allowed bool, err error) {
-	con := a.client
-	accessReview := con.Authorizations().V1().AccessReview()
-
-	request, err := azv1.NewAccessReviewRequest().
-		AccountUsername(username).
-		Action(action).
-		ResourceType(resourceType).
-		OrganizationID(organizationID).
-		ClusterID(clusterID).
-		SubscriptionID(subscriptionID).
-		Build()
-	if err != nil {
-		return false, err
-	}
-
-	postResp, err := accessReview.Post().
-		Request(request).
-		SendContext(ctx)
-	if err != nil {
-		return false, err
-	}
-
-	response, ok := postResp.GetResponse()
-	if !ok {
-		return false, fmt.Errorf("Empty response from authorization post request")
-	}
-
-	return response.Allowed(), nil
 }
 
 func (a authorization) CheckUserValid(username string, orgId string) (bool, error) {
