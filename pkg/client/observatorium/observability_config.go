fa0ccc1e0353ade3d9501f6bec0b886f7059711a
Manyanda Chitimbo <mchitimb@redhat.com>
2021-08-06 14:20:50 +0200

    feat(observability): update observability resource repo tag

--- pkg/client/observatorium/observability_config.go
+++ pkg/client/observatorium/observability_config.go
@@ -75,7 +75,7 @@ func NewObservabilityConfigurationConfig() *ObservabilityConfiguration {
 		ObservabilityConfigChannel:         "resources", // Pointing to resources as the individual directories for prod and staging are no longer needed
 		ObservabilityConfigAccessToken:     "",
 		ObservabilityConfigAccessTokenFile: "secrets/observability-config-access.token",
-		ObservabilityConfigTag:             "v1.5.0-staging",
+		ObservabilityConfigTag:             "v1.8.1-staging",
 		MetricsClientIdFile:                "secrets/rhsso-metrics.clientId",
 		MetricsSecretFile:                  "secrets/rhsso-metrics.clientSecret",
 		LogsClientIdFile:                   "secrets/rhsso-logs.clientId",
