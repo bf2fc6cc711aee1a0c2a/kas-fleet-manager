684732277e8d557d20f79178428316f07deef6d8
Massimiliano Ziccardi <mziccard@redhat.com>
2021-08-17 16:53:53 +0200

    fix: 🐛 implemented quota check as suggested by ams team
    Current quota check implementation was relying on the quota_id. This
    change so that the preferred methos is used (related resources)

--- pkg/client/ocm/types.go
+++ pkg/client/ocm/types.go
@@ -4,3 +4,26 @@ type Parameter struct {
 	Id    string
 	Value string
 }
+
+type KafkaQuotaType string
+
+const (
+	EvalQuota     KafkaQuotaType = "eval"
+	StandardQuota KafkaQuotaType = "standard"
+)
+
+func (t KafkaQuotaType) GetProduct() string {
+	if t == StandardQuota {
+		return "RHOSAK"
+	}
+
+	return "RHOSAKTrial"
+}
+
+func (t KafkaQuotaType) GetResourceName() string {
+	return "rhosak"
+}
+
+func (t KafkaQuotaType) Equals(t1 KafkaQuotaType) bool {
+	return t1.GetProduct() == t.GetProduct() && t1.GetResourceName() == t.GetResourceName()
+}
