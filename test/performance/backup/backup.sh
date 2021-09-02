f95325ae2ae49cd7df83c6fef979ef8ab4312949
Pawel Paszki <ppaszki@redhat.com>
2021-09-02 07:41:00 +0100

    feat: changed base image of perf test backup image (#565)

--- test/performance/backup/backup.sh
+++ test/performance/backup/backup.sh
@@ -3,7 +3,7 @@
 export AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY"
 export AWS_SECRET_ACCESS_KEY="$AWS_SECRET"
 
-FILENAME="$(date +%Y-%m-%d_%H-%M-%S)"
+FILENAME="$(date +%Y-%m-%d-%H-%M-%S)"
 KUBECONFIG_LOCATION="/usr/local/bin/.kubeconfig"
 
 DUMP_FILENAME="/usr/local/bin/$FILENAME"
@@ -14,8 +14,10 @@ oc --kubeconfig "$KUBECONFIG_LOCATION" project "$BACKUP_PROJECT"
 
 oc --kubeconfig "$KUBECONFIG_LOCATION" exec "$DB_POD_NAME" -- bash -c "pg_dump $DB_NAME -U $DB_USER > /var/lib/pgsql/data/$FILENAME"
 
-oc --kubeconfig "$KUBECONFIG_LOCATION" cp "$DB_POD_NAME:/var/lib/pgsql/data/$FILENAME" "$DUMP_FILENAME"
+oc --kubeconfig "$KUBECONFIG_LOCATION" exec "$DB_POD_NAME" -- tar -czf /var/lib/pgsql/data/"$FILENAME".tgz "/var/lib/pgsql/data/$FILENAME"
 
-aws s3 cp "$DUMP_FILENAME" "s3://$S3_BUCKET_NAME"
+oc --kubeconfig "$KUBECONFIG_LOCATION" cp "$DB_POD_NAME:/var/lib/pgsql/data/$FILENAME.tgz" "$DUMP_FILENAME.tgz"
 
-oc --kubeconfig "$KUBECONFIG_LOCATION" exec "$DB_POD_NAME" -- bash -c "rm /var/lib/pgsql/data/$FILENAME"
+aws s3 cp "$DUMP_FILENAME.tgz" "s3://$S3_BUCKET_NAME"
+
+oc --kubeconfig "$KUBECONFIG_LOCATION" exec "$DB_POD_NAME" -- bash -c "rm /var/lib/pgsql/data/$FILENAME.tgz"
