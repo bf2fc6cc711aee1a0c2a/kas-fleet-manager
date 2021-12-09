4e1b8c030fc40fc43abc8b6e8b242fe8a93a9aca
Vincent Manley <vmanley@redhat.com>
2021-12-09 10:28:14 +0000

    feat: update observability operator to v3.0.8

--- internal/kafka/internal/workers/clusters_mgr_test.go
+++ internal/kafka/internal/workers/clusters_mgr_test.go
@@ -1264,7 +1264,7 @@ func buildResourceSet(observabilityConfig observatorium.ObservabilityConfigurati
 				CatalogSource:          observabilityCatalogSourceName,
 				Channel:                "alpha",
 				CatalogSourceNamespace: observabilityNamespace,
-				StartingCSV:            "observability-operator.v3.0.7",
+				StartingCSV:            "observability-operator.v3.0.8",
 				InstallPlanApproval:    v1alpha1.ApprovalAutomatic,
 				Package:                observabilitySubscriptionName,
 			},
