95526c9c9ce88e2fd8d7422487d0bc4c065f57d0
Avinash Dongre <adongre@adongre.pnq.csb>
2022-02-04 16:27:43 +0530

    refactor: MGDSTRM-7182, Remove temporary code for cluster assignment in accepted kafkas reconciler

--- internal/kafka/internal/workers/kafka_mgrs/accepted_kafkas_mgr.go
+++ internal/kafka/internal/workers/kafka_mgrs/accepted_kafkas_mgr.go
@@ -82,15 +82,10 @@ func (k *AcceptedKafkaManager) Reconcile() []error {
 }
 
 func (k *AcceptedKafkaManager) reconcileAcceptedKafka(kafka *dbapi.KafkaRequest) error {
-	var cluster *api.Cluster
-
-	foundCluster, e := k.clusterService.FindClusterByID(kafka.ClusterID)
-
-	if foundCluster == nil || e != nil {
+	cluster, e := k.clusterService.FindClusterByID(kafka.ClusterID)
+	if cluster == nil || e != nil {
 		return errors.Wrapf(e, "failed to find cluster with '%s' for kafka request '%s'", kafka.ClusterID, kafka.ID)
 	}
-	cluster = foundCluster
-
 	// Set desired Strimzi version
 	var selectedStrimziVersion *api.StrimziVersion
 
