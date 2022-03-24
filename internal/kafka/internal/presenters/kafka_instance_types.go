d39a3aa8cc80c52c2bdbfdf0f949979719b1dc23
Vincent Manley <vmanley@redhat.com>
2022-03-24 11:46:13 +0000

    refactor: remove Kind and rename Items to InstanceTypes

--- internal/kafka/internal/presenters/kafka_instance_types.go
+++ internal/kafka/internal/presenters/kafka_instance_types.go
@@ -8,7 +8,6 @@ import (
 func PresentSupportedKafkaInstanceType(supportedInstanceType *api.SupportedKafkaInstanceType) public.SupportedKafkaInstanceType {
 	reference := PresentReference(supportedInstanceType.Id, supportedInstanceType)
 	return public.SupportedKafkaInstanceType{
-		Kind:  reference.Kind,
 		Id:    reference.Id,
 		Sizes: GetSupportedKafkaSizes(supportedInstanceType.SupportedKafkaSizes),
 	}
@@ -17,7 +16,7 @@ func PresentSupportedKafkaInstanceType(supportedInstanceType *api.SupportedKafka
 func GetSupportedKafkaSizes(supportedKafkaSizes []api.SupportedKafkaSize) []public.SupportedKafkaSize {
 	sizes := make([]public.SupportedKafkaSize, 0)
 	for _, c := range supportedKafkaSizes {
-		sizes = append(sizes, public.SupportedKafkaSize{Kind: c.Kind, Id: c.Id, IngressThroughputPerSec: c.IngressThroughputPerSec, EgressThroughputPerSec: c.EgressThroughputPerSec, TotalMaxConnections: c.TotalMaxConnections, MaxDataRetentionSize: c.MaxDataRetentionSize, MaxPartitions: c.MaxPartitions, MaxDataRetentionPeriod: c.MaxDataRetentionPeriod, MaxConnectionAttemptsPerSec: c.MaxConnectionAttemptsPerSec, QuotaConsumed: c.QuotaConsumed, QuotaType: c.QuotaType, CapacityConsumed: c.CapacityConsumed})
+		sizes = append(sizes, public.SupportedKafkaSize{Id: c.Id, IngressThroughputPerSec: c.IngressThroughputPerSec, EgressThroughputPerSec: c.EgressThroughputPerSec, TotalMaxConnections: c.TotalMaxConnections, MaxDataRetentionSize: c.MaxDataRetentionSize, MaxPartitions: c.MaxPartitions, MaxDataRetentionPeriod: c.MaxDataRetentionPeriod, MaxConnectionAttemptsPerSec: c.MaxConnectionAttemptsPerSec, QuotaConsumed: c.QuotaConsumed, QuotaType: c.QuotaType, CapacityConsumed: c.CapacityConsumed})
 	}
 	return sizes
 }
