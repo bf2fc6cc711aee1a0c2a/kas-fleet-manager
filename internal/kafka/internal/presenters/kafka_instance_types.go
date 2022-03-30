51e7c5599d8ce5c793e1eeab00b418eb1bdd88ec
Vincent Manley <vmanley@redhat.com>
2022-03-30 15:05:46 +0100

    refactor: handling of changed endpoint return& minor fixes

--- internal/kafka/internal/presenters/kafka_instance_types.go
+++ internal/kafka/internal/presenters/kafka_instance_types.go
@@ -16,7 +16,7 @@ func PresentSupportedKafkaInstanceType(supportedInstanceType *api.SupportedKafka
 func GetSupportedKafkaSizes(supportedKafkaSizes []api.SupportedKafkaSize) []public.SupportedKafkaSize {
 	sizes := make([]public.SupportedKafkaSize, 0)
 	for _, c := range supportedKafkaSizes {
-		sizes = append(sizes, public.SupportedKafkaSize{Id: c.Id, IngressThroughputPerSec: c.IngressThroughputPerSec, EgressThroughputPerSec: c.EgressThroughputPerSec, TotalMaxConnections: c.TotalMaxConnections, MaxDataRetentionSize: c.MaxDataRetentionSize, MaxPartitions: c.MaxPartitions, MaxDataRetentionPeriod: c.MaxDataRetentionPeriod, MaxConnectionAttemptsPerSec: c.MaxConnectionAttemptsPerSec, QuotaConsumed: c.QuotaConsumed, QuotaType: c.QuotaType, CapacityConsumed: c.CapacityConsumed})
+		sizes = append(sizes, public.SupportedKafkaSize{Id: c.Id, IngressThroughputPerSec: public.SupportedKafkaSizeBytesValueItem(c.IngressThroughputPerSec), EgressThroughputPerSec: public.SupportedKafkaSizeBytesValueItem(c.EgressThroughputPerSec), TotalMaxConnections: c.TotalMaxConnections, MaxDataRetentionSize: public.SupportedKafkaSizeBytesValueItem(c.MaxDataRetentionSize), MaxPartitions: c.MaxPartitions, MaxDataRetentionPeriod: c.MaxDataRetentionPeriod, MaxConnectionAttemptsPerSec: c.MaxConnectionAttemptsPerSec, QuotaConsumed: c.QuotaConsumed, QuotaType: c.QuotaType, CapacityConsumed: c.CapacityConsumed})
 	}
 	return sizes
 }
