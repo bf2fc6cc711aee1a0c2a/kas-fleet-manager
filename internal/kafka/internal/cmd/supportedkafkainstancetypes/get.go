c3c5baaa0f5684cec9c5b499f0b6b2472861a2af
Vincent Manley <vmanley@redhat.com>
2022-04-04 15:22:01 +0100

    refactor: remove structs from pkg/api and use internal/kafka/internal/api/public types

--- internal/kafka/internal/cmd/supportedkafkainstancetypes/get.go
+++ internal/kafka/internal/cmd/supportedkafkainstancetypes/get.go
@@ -4,8 +4,6 @@ import (
 	"encoding/json"
 
 	"github.com/bf2fc6cc711aee1a0c2a/kas-fleet-manager/internal/kafka/internal/api/public"
-	"github.com/bf2fc6cc711aee1a0c2a/kas-fleet-manager/internal/kafka/internal/config"
-	"github.com/bf2fc6cc711aee1a0c2a/kas-fleet-manager/internal/kafka/internal/presenters"
 	"github.com/bf2fc6cc711aee1a0c2a/kas-fleet-manager/internal/kafka/internal/services"
 	"github.com/bf2fc6cc711aee1a0c2a/kas-fleet-manager/pkg/environments"
 	"github.com/bf2fc6cc711aee1a0c2a/kas-fleet-manager/pkg/flags"
@@ -30,10 +28,9 @@ func NewGetCommand(env *environments.Env) *cobra.Command {
 
 func runGet(env *environments.Env, cmd *cobra.Command, _ []string) {
 	id := flags.MustGetDefinedString(FlagCloudProvider, cmd.Flags())
-	region := flags.MustGetDefinedString(FlagCloudProvider, cmd.Flags())
+	region := flags.MustGetDefinedString(FlagCloudRegion, cmd.Flags())
 	var supportedKafkaInstanceTypeService services.SupportedKafkaInstanceTypesService
-	var kafkaConfig *config.KafkaConfig
-	env.MustResolveAll(&supportedKafkaInstanceTypeService, &kafkaConfig)
+	env.MustResolve(&supportedKafkaInstanceTypeService)
 
 	regionInstanceTypeList, err := supportedKafkaInstanceTypeService.GetSupportedKafkaInstanceTypesByRegion(id, region)
 	if err != nil {
@@ -45,8 +42,7 @@ func runGet(env *environments.Env, cmd *cobra.Command, _ []string) {
 	}
 
 	for _, regionInstanceType := range regionInstanceTypeList {
-		converted := presenters.PresentSupportedKafkaInstanceType(&regionInstanceType)
-		supportedKafkaInstanceTypeList.InstanceTypes = append(supportedKafkaInstanceTypeList.InstanceTypes, converted)
+		supportedKafkaInstanceTypeList.InstanceTypes = append(supportedKafkaInstanceTypeList.InstanceTypes, regionInstanceType)
 	}
 
 	output, marshalErr := json.MarshalIndent(supportedKafkaInstanceTypeList, "", "    ")
