14000f5fc8fc1dc55ebc9560b810a7340506a233
Vincent Manley <vmanley@redhat.com>
2022-04-07 14:28:04 +0100

    refactor: move code from services to new presenter

--- internal/kafka/internal/services/kafka_instance_types.go
+++ internal/kafka/internal/services/kafka_instance_types.go
@@ -4,14 +4,13 @@ import (
 	"fmt"
 	"sort"
 
-	"github.com/bf2fc6cc711aee1a0c2a/kas-fleet-manager/internal/kafka/internal/api/public"
 	"github.com/bf2fc6cc711aee1a0c2a/kas-fleet-manager/internal/kafka/internal/config"
 	"github.com/bf2fc6cc711aee1a0c2a/kas-fleet-manager/pkg/errors"
 )
 
 //go:generate moq -out kafka_instance_types_moq.go . SupportedKafkaInstanceTypesService
 type SupportedKafkaInstanceTypesService interface {
-	GetSupportedKafkaInstanceTypesByRegion(providerId string, regionId string) ([]public.SupportedKafkaInstanceType, *errors.ServiceError)
+	GetSupportedKafkaInstanceTypesByRegion(providerId string, regionId string) ([]config.KafkaInstanceType, *errors.ServiceError)
 }
 
 type supportedKafkaInstanceTypesService struct {
@@ -26,8 +25,8 @@ func NewSupportedKafkaInstanceTypesService(providerConfig *config.ProviderConfig
 	}
 }
 
-func (t supportedKafkaInstanceTypesService) GetSupportedKafkaInstanceTypesByRegion(providerId string, regionId string) ([]public.SupportedKafkaInstanceType, *errors.ServiceError) {
-	instanceTypeList := []public.SupportedKafkaInstanceType{}
+func (t supportedKafkaInstanceTypesService) GetSupportedKafkaInstanceTypesByRegion(providerId string, regionId string) ([]config.KafkaInstanceType, *errors.ServiceError) {
+	instanceTypeList := []config.KafkaInstanceType{}
 	provider, providerFound := t.providerConfig.ProvidersConfig.SupportedProviders.GetByName(providerId)
 	if !providerFound {
 		return nil, errors.ProviderNotSupported(fmt.Sprintf("cloud provider '%s' is unsupported", providerId))
@@ -43,37 +42,11 @@ func (t supportedKafkaInstanceTypesService) GetSupportedKafkaInstanceTypesByRegi
 		if err != nil {
 			return nil, errors.InstanceTypeNotSupported(fmt.Sprintf("instance type '%s' is unsupported", k))
 		}
-		sizes := instanceType.Sizes
-		supportedSizesList := []public.SupportedKafkaSize{}
-		for _, size := range sizes {
-			//errors from Quantity.ToFloat32() ignored. Strings already validated as resource.Quantity
-			ingressBytes, _ := size.IngressThroughputPerSec.ToFloat32()
-			egressBytes, _ := size.EgressThroughputPerSec.ToFloat32()
-			retentionSizeBytes, _ := size.MaxDataRetentionSize.ToFloat32()
-			supportedSizesList = append(supportedSizesList, public.SupportedKafkaSize{
-				Id: size.Id,
-				IngressThroughputPerSec: public.SupportedKafkaSizeBytesValueItem{
-					Bytes: ingressBytes,
-				},
-				EgressThroughputPerSec: public.SupportedKafkaSizeBytesValueItem{
-					Bytes: egressBytes,
-				},
-				TotalMaxConnections: int32(size.TotalMaxConnections),
-				MaxDataRetentionSize: public.SupportedKafkaSizeBytesValueItem{
-					Bytes: retentionSizeBytes,
-				},
-				MaxPartitions:               int32(size.MaxPartitions),
-				MaxDataRetentionPeriod:      size.MaxDataRetentionPeriod,
-				MaxConnectionAttemptsPerSec: int32(size.MaxConnectionAttemptsPerSec),
-				QuotaConsumed:               int32(size.QuotaConsumed),
-				QuotaType:                   size.QuotaType,
-				CapacityConsumed:            int32(size.CapacityConsumed),
-			})
-		}
-		instanceTypeList = append(instanceTypeList, public.SupportedKafkaInstanceType{
+
+		instanceTypeList = append(instanceTypeList, config.KafkaInstanceType{
 			Id:          k,
 			DisplayName: instanceType.DisplayName,
-			Sizes:       supportedSizesList,
+			Sizes:       instanceType.Sizes,
 		})
 	}
 	sort.Slice(instanceTypeList, func(i, j int) bool {
