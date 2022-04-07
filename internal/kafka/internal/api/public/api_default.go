1c79ebacb3003824bc6fd064dfe2a12142874a4e
Vincent Manley <vmanley@redhat.com>
2022-04-07 16:07:37 +0100

    fix: include 400 error response and examples in openapi spec

--- internal/kafka/internal/api/public/api_default.go
+++ internal/kafka/internal/api/public/api_default.go
@@ -698,6 +698,16 @@ func (a *DefaultApiService) GetInstanceTypesByCloudProviderAndRegion(ctx _contex
 			body:  localVarBody,
 			error: localVarHTTPResponse.Status,
 		}
+		if localVarHTTPResponse.StatusCode == 400 {
+			var v Error
+			err = a.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
+				return localVarReturnValue, localVarHTTPResponse, newErr
+			}
+			newErr.model = v
+			return localVarReturnValue, localVarHTTPResponse, newErr
+		}
 		if localVarHTTPResponse.StatusCode == 401 {
 			var v Error
 			err = a.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
