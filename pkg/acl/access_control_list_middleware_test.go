7ea264a7bf93321fb7eb4341078076d32cb77dc1
Pawel Paszki <pawelpaszki@gmail.com>
2022-04-13 09:06:50 +0100

    feat: increased unit test coverage in the pkg/acl package (#919)
    * feat: increased unit test coverage in the pkg/acl package
    
    * feat: added missing test cases
    
    * feat: changed handling errors in pkg/acl tests

--- pkg/acl/access_control_list_middleware_test.go
+++ pkg/acl/access_control_list_middleware_test.go
@@ -11,7 +11,6 @@ import (
 
 	"github.com/bf2fc6cc711aee1a0c2a/kas-fleet-manager/internal/kafka"
 	"github.com/bf2fc6cc711aee1a0c2a/kas-fleet-manager/pkg/acl"
-	"github.com/bf2fc6cc711aee1a0c2a/kas-fleet-manager/pkg/errors"
 	"github.com/bf2fc6cc711aee1a0c2a/kas-fleet-manager/pkg/server"
 	"github.com/golang/glog"
 
@@ -42,15 +41,15 @@ func TestMain(m *testing.M) {
 }
 
 func Test_AccessControlListMiddleware_UserHasNoAccess(t *testing.T) {
+	RegisterTestingT(t)
 	authHelper, err := auth.NewAuthHelper(jwtKeyFile, jwtCAFile, serverConfig.TokenIssuerURL)
-	if err != nil {
-		t.Fatal(err)
-	}
+	Expect(err).NotTo(HaveOccurred())
 
 	tests := []struct {
-		name string
-		arg  *acl.AccessControlListConfig
-		want *errors.ServiceError
+		name           string
+		arg            *acl.AccessControlListConfig
+		wantErr        bool
+		wantHttpStatus int
 	}{
 		{
 			name: "returns 403 Forbidden response when user is not allowed to access service",
@@ -58,17 +57,32 @@ func Test_AccessControlListMiddleware_UserHasNoAccess(t *testing.T) {
 				EnableDenyList: true,
 				DenyList:       acl.DeniedUsers{"username"},
 			},
+			wantErr:        true,
+			wantHttpStatus: http.StatusForbidden,
+		},
+		{
+			name: "returns 200 status if denyList is disabled",
+			arg: &acl.AccessControlListConfig{
+				EnableDenyList: false,
+			},
+			wantErr:        false,
+			wantHttpStatus: http.StatusOK,
+		},
+		{
+			name: "returns 200 status if denyList is enabled and deny list is empty",
+			arg: &acl.AccessControlListConfig{
+				EnableDenyList: true,
+			},
+			wantErr:        false,
+			wantHttpStatus: http.StatusOK,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			RegisterTestingT(t)
 
 			req, err := http.NewRequest("GET", "/api/kafkas_mgmt/kafkas", nil)
-			if err != nil {
-				t.Fatal(err)
-			}
+			Expect(err).NotTo(HaveOccurred())
 
 			rr := httptest.NewRecorder()
 
@@ -78,37 +92,30 @@ func Test_AccessControlListMiddleware_UserHasNoAccess(t *testing.T) {
 			// create a jwt and set it in the context
 			ctx := req.Context()
 			acc, err := authHelper.NewAccount("username", "test-user", "", "org-id-0")
-			if err != nil {
-				t.Fatal(err)
-			}
+			Expect(err).NotTo(HaveOccurred())
 
 			token, err := authHelper.CreateJWTWithClaims(acc, nil)
-			if err != nil {
-				t.Fatal(err)
-			}
+			Expect(err).NotTo(HaveOccurred())
 
 			ctx = auth.SetTokenInContext(ctx, token)
 			req = req.WithContext(ctx)
 			handler.ServeHTTP(rr, req)
 
 			body, err := ioutil.ReadAll(rr.Body)
-			if err != nil {
-				t.Fatal(err)
-			}
+			Expect(err).NotTo(HaveOccurred())
+			Expect(rr.Code).To(Equal(tt.wantHttpStatus))
 
-			Expect(rr.Code).To(Equal(http.StatusForbidden))
-			Expect(rr.Header().Get("Content-Type")).To(Equal("application/json"))
-
-			var data map[string]string
-			err = json.Unmarshal(body, &data)
-			if err != nil {
-				t.Fatal(err)
+			if tt.wantErr {
+				Expect(rr.Header().Get("Content-Type")).To(Equal("application/json"))
+				var data map[string]string
+				err = json.Unmarshal(body, &data)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(data["kind"]).To(Equal("Error"))
+				Expect(data["reason"]).To(Equal("User 'username' is not authorized to access the service."))
+				// verify that context about user being allowed as service account is set to false always
+				ctxAfterMiddleware := req.Context()
+				Expect(auth.GetFilterByOrganisationFromContext(ctxAfterMiddleware)).To(Equal(false))
 			}
-			Expect(data["kind"]).To(Equal("Error"))
-			Expect(data["reason"]).To(Equal("User 'username' is not authorized to access the service."))
-			// verify that context about user being allowed as service account is set to false always
-			ctxAfterMiddleware := req.Context()
-			Expect(auth.GetFilterByOrganisationFromContext(ctxAfterMiddleware)).To(Equal(false))
 		})
 	}
 }
@@ -117,7 +124,5 @@ func Test_AccessControlListMiddleware_UserHasNoAccess(t *testing.T) {
 func NextHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK) //nolint
 	_, err := io.WriteString(w, "OK")
-	if err != nil {
-		panic(err)
-	}
+	Expect(err).NotTo(HaveOccurred())
 }
--- pkg/acl/access_control_list_test.go
+++ pkg/acl/access_control_list_test.go
@@ -34,13 +34,116 @@ func Test_DeniedUsers_IsUserDenied(t *testing.T) {
 			want:           true,
 		},
 	}
+	RegisterTestingT(t)
 	for _, tt := range tests {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
-			RegisterTestingT(t)
 			ok := tt.deniedAccounts.IsUserDenied(tt.arg)
 			Expect(ok).To(Equal(tt.want))
+		})
+	}
+}
+
+func Test_ReadDenyListConfigFile(t *testing.T) {
+	type args struct {
+		file        string
+		deniedUsers *DeniedUsers
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "should return an error when attempting to read deny list with non-existent file",
+			args: args{
+				file:        "non-existent path",
+				deniedUsers: &DeniedUsers{},
+			},
+			wantErr: true,
+		},
+		{
+			name: "should not return an error when attempting to read deny list with non-existent file",
+			args: args{
+				file:        "config/deny-list-configuration.yaml",
+				deniedUsers: &DeniedUsers{},
+			},
+			wantErr: false,
+		},
+	}
+
+	RegisterTestingT(t)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := readDenyListConfigFile(tt.args.file, tt.args.deniedUsers)
+			Expect(err != nil).To(Equal(tt.wantErr))
+		})
+	}
+}
+
+func Test_ReadFiles(t *testing.T) {
+	type args struct {
+		file           string
+		deniedUsers    *DeniedUsers
+		enableDenyList bool
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "should not return an error when deny list is enabled and deny list config file path is valid",
+			args: args{
+				file:           "config/deny-list-configuration.yaml",
+				deniedUsers:    &DeniedUsers{},
+				enableDenyList: true,
+			},
+			wantErr: false,
+		},
+		{
+			name: "should return an error when deny list is enabled and config file path is invalid",
+			args: args{
+				file:           "invalid-path",
+				deniedUsers:    &DeniedUsers{},
+				enableDenyList: true,
+			},
+			wantErr: true,
+		},
+		{
+			name: "should not return an error when deny list is disabled",
+			args: args{
+				file:           "config/deny-list-configuration.yaml",
+				deniedUsers:    &DeniedUsers{},
+				enableDenyList: false,
+			},
+			wantErr: false,
+		},
+		{
+			name: "should not return an error when deny list is disabled and deny list config file path is invalid",
+			args: args{
+				file:           "config/deny-list-configuration.yaml",
+				deniedUsers:    &DeniedUsers{},
+				enableDenyList: false,
+			},
+			wantErr: false,
+		},
+	}
+
+	RegisterTestingT(t)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			aclConfig := NewAccessControlListConfig()
+			aclConfig.DenyList = *tt.args.deniedUsers
+			aclConfig.EnableDenyList = tt.args.enableDenyList
+			aclConfig.DenyListConfigFile = tt.args.file
+			err := aclConfig.ReadFiles()
+			Expect(err != nil).To(Equal(tt.wantErr))
 		})
 	}
 }
