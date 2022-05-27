c7dfb22b6527e4c9c99190cfa54b48e7a9c24a5f
Eoin Gallinagh <eoingallinagh@gmail.com>
2022-05-27 17:11:17 +0100

    test: Increase unit test code coverage in pkg/services/sentry

--- pkg/services/sentry/config_test.go
+++ pkg/services/sentry/config_test.go
@@ -7,26 +7,38 @@ import (
 )
 
 func Test_Sentry_ReadFiles(t *testing.T) {
-	configWithSentryDisabled := NewConfig()
-	configWithSentryDisabled.Enabled = false
-	configWithSentryDisabled.KeyFile = "a file that does not exists so that to show that we are not performing file reading"
 	tests := []struct {
-		name    string
-		config  *Config
-		wantErr bool
+		name     string
+		modifyFn func(config *Config)
+		wantErr  bool
 	}{
 		{
-			name:    "Do not read file when sentry disabled",
+			name: "Do not read file when sentry disabled",
+			modifyFn: func(config *Config) {
+				config.Enabled = false
+				config.KeyFile = "a file that does not exists so that to show that we are not performing file reading"
+			},
 			wantErr: false,
-			config:  configWithSentryDisabled,
+		},
+		{
+			name: "Should return an error with non-existent KeyFile and sentry enabled.",
+			modifyFn: func(config *Config) {
+				config.Enabled = true
+				config.KeyFile = "a file that does not exists to show that we will perform a file reading with sentry enabled and return error"
+			},
+			wantErr: true,
 		},
 	}
 
 	RegisterTestingT(t)
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			err := tt.config.ReadFiles()
-			Expect(tt.wantErr).To(Equal(err != nil))
+			config := NewConfig()
+			if tt.modifyFn != nil {
+				tt.modifyFn(config)
+			}
+			Expect(config.ReadFiles() != nil).To(Equal(tt.wantErr))
 		})
 	}
 }
---
+++ pkg/services/sentry/sentry_test.go
@@ -0,0 +1,77 @@
+package sentry
+
+import (
+	"testing"
+	"time"
+
+	"github.com/bf2fc6cc711aee1a0c2a/kas-fleet-manager/pkg/environments"
+	"github.com/getsentry/sentry-go"
+	. "github.com/onsi/gomega"
+)
+
+func TestInitialize(t *testing.T) {
+	type args struct {
+		envName environments.EnvName
+		c       *Config
+	}
+	tests := []struct {
+		name string
+		args args
+		want error
+	}{
+		{
+			name: "Return error when sentry error monitoring is enabled and project config is invalid",
+			args: args{
+				envName: environments.EnvName("testEnv"),
+				c: &Config{
+					Enabled: true,
+					Key:     "1234",
+					URL:     "test.url",
+					Project: "invalid-id",
+					Debug:   true,
+					Timeout: time.Hour,
+					KeyFile: "secrets/sentry.key",
+				},
+			},
+			want: &sentry.DsnParseError{Message: "invalid project id"},
+		},
+		{
+			name: "Return nil with sentry error monitoring disabled",
+			args: args{
+				envName: environments.EnvName("testEnv"),
+				c: &Config{
+					Enabled: false,
+				},
+			},
+			want: nil,
+		},
+		{
+			name: "Return nil when sentry config is enabled and config is valid",
+			args: args{
+				envName: environments.EnvName("testEnv"),
+				c: &Config{
+					Enabled: true,
+					Key:     "1234",
+					URL:     "https//:test-url.domain",
+					Project: "3",
+					Debug:   true,
+					Timeout: time.Hour,
+					KeyFile: "secrets/sentry.key",
+				},
+			},
+			want: nil,
+		},
+	}
+
+	RegisterTestingT(t)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := Initialize(tt.args.envName, tt.args.c)
+			Expect(got == nil).To(Equal(tt.want == nil))
+			if got != nil {
+				Expect(got).To(Equal(tt.want))
+			}
+		})
+	}
+}
