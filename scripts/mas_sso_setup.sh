41ca4d657372a66cb53bcb5b80ef91a12fcb5300
valeriiashapoval <shapoval.valeriia@outlook.com>
2022-06-20 10:22:28 +0100

    addressed PR comments

--- scripts/mas_sso_setup.sh
+++ scripts/mas_sso_setup.sh
@@ -9,6 +9,6 @@ docker run \
   --net mas-sso-network \
   -p $KEYCLOAK_PORT_NO:8080 \
   -e DB_VENDOR=h2  \
-  -e KEYCLOAK_USER=$KEYCLOAK_USER \
-  -e KEYCLOAK_PASSWORD=$KEYCLOAK_USER \
+  -e KEYCLOAK_USER=${KEYCLOAK_USER} \
+  -e KEYCLOAK_PASSWORD=${KEYCLOAK_PASSWORD} \
   -d quay.io/keycloak/keycloak:17.0.1-legacy
