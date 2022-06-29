aa15b87544565ac71e69f82d9026784e4b4e9963
Pawel Paszki <ppaszki@redhat.com>
2022-06-29 11:58:36 +0100

    feat: renamed branch to more meaningful name

--- build_deploy.sh
+++ build_deploy.sh
@@ -112,11 +112,12 @@ if [[ -n "$AUTHOR_EMAIL" ]] && [[ -n "$AUTHOR_NAME" ]] && [[ -n "$GITLAB_TOKEN" 
   LATEST_COMMIT=$(git rev-parse HEAD)
   git clone "https://gitlab-ci-token:$GITLAB_TOKEN@gitlab.cee.redhat.com/mk-ci-cd/managed-kafka-versions.git" managed-kafka-versions
   cd managed-kafka-versions
+  BRANCH_NAME="kas-fleet-manager-${VERSION}"
   # only update the config, if different
   CURRENT_COMMIT_SHA=$(yq '.service.scm.commitSha' services/kas-fleet-manager.yaml)
   echo "Checking if the latest commit sha: $LATEST_COMMIT is different than current config commit sha: $CURRENT_COMMIT_SHA"
   if [[ "${CURRENT_COMMIT_SHA}" != "${LATEST_COMMIT}" ]]; then
-    git checkout -b "$VERSION"
+    git checkout -b "$BRANCH_NAME"
     echo "Updating commit sha and image tag for kas-fleet-manager configuration"
     # update commitSha
     yq -i ".service.scm.commitSha = \"$LATEST_COMMIT\"" services/kas-fleet-manager.yaml
@@ -127,7 +128,7 @@ if [[ -n "$AUTHOR_EMAIL" ]] && [[ -n "$AUTHOR_NAME" ]] && [[ -n "$GITLAB_TOKEN" 
     git commit -a -m "kas-fleet-manager stage release $VERSION"
     # create an MR in managed-kafka-versions repo
     echo "Creating MR with new config in managed-kafka-versions repository"
-    git push --force -o merge_request.create="true" -o merge_request.title="kas-fleet-manager stage release $VERSION" -o merge_request.description="https://gitlab.cee.redhat.com/service/kas-fleet-manager/-/compare/$CURRENT_COMMIT_SHA...$LATEST_COMMIT" -o merge_request.merge_when_pipeline_succeeds="false" -u origin "$VERSION"
+    git push --force -o merge_request.create="true" -o merge_request.title="kas-fleet-manager stage release $VERSION" -o merge_request.description="https://gitlab.cee.redhat.com/service/kas-fleet-manager/-/compare/$CURRENT_COMMIT_SHA...$LATEST_COMMIT" -o merge_request.merge_when_pipeline_succeeds="false" -u origin "$BRANCH_NAME"
   else
     echo "No new version detected for kas-fleet-manager"
   fi
