528a9ea509c23085a9fc137048ab0c3f5667f215
Pawel Paszki <ppaszki@redhat.com>
2022-07-18 15:32:26 +0100

    feat: added auto approving mrs created in managed-kafka-versions

--- create_release.sh
+++ create_release.sh
@@ -18,7 +18,7 @@ if [[ "${CURRENT_COMMIT_SHA}" != "${LATEST_COMMIT}" ]]; then
   git commit -a -m "kas-fleet-manager stage release $VERSION"
   # create an MR in managed-kafka-versions repo
   echo "Creating MR with new config in managed-kafka-versions repository"
-  git push --force -o merge_request.create="true" -o merge_request.title="kas-fleet-manager stage release $VERSION" -o merge_request.description="https://gitlab.cee.redhat.com/service/kas-fleet-manager/-/compare/$CURRENT_COMMIT_SHA...$LATEST_COMMIT" -o merge_request.merge_when_pipeline_succeeds="false" -u origin "$BRANCH_NAME"
+  git push --force -o merge_request.create="true" -o merge_request.title="kas-fleet-manager stage release $VERSION" -o merge_request.description="https://gitlab.cee.redhat.com/service/kas-fleet-manager/-/compare/$CURRENT_COMMIT_SHA...$LATEST_COMMIT" -o merge_request.merge_when_pipeline_succeeds="true" -u origin "$BRANCH_NAME"
 else
   echo "No new version detected for kas-fleet-manager"
 fi
