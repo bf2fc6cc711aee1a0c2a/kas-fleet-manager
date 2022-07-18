36e5391cbd9d6f58c8d81f05a515d35508ef1336
Miguel Soriano <msoriano@redhat.com>
2022-07-18 16:25:55 +0200

    feat: add minimum available capacity slack concept
    As part of dynamic scaling, the concept of a minimum
    available capacity slack in streaming units has been introduced.
    The available capacity slack of a supported instance type in a region
    is the desired minimum number of streaming units that should
    be available(free) at any moment for that instance type in the region.
    
    It has been introduced in the provider configuration file of the
    fleet manager. There is one capacity slack value per defined instance
    type in the region.
    The attribute is optional. If it is not set then it is interpreted
    as a capacity slack of 0 is desired.
    The capacity slack is intended to be used only when dynamic scaling
    is enabled.

--- internal/kafka/internal/config/providers.go
+++ internal/kafka/internal/config/providers.go
@@ -17,7 +17,13 @@ type InstanceType types.KafkaInstanceType
 type InstanceTypeMap map[string]InstanceTypeConfig
 
 type InstanceTypeConfig struct {
-	Limit *int `yaml:"limit,omitempty"`
+	Limit *int `yaml:"limit"`
+	// Minimum capacity in number of kafka streaming units that should be
+	// available (free) at any given moment for a supported instance type in a
+	// region. If not provided, its default value is 0 which means that there is
+	// no minimum available capacity required.
+	// Used for dynamic scaling evaluation.
+	MinAvailableCapacitySlackStreamingUnits int `yaml:"min_available_capacity_slack_streaming_units"`
 }
 
 // Returns a region's supported instance type list as a slice
@@ -60,38 +66,50 @@ func (r Region) Validate(dataplaneClusterConfig *DataplaneClusterConfig) error {
 
 	// verify that Limits set in this configuration matches the capacity of clusters listed in the data plane configuration
 	for k, v := range r.SupportedInstanceTypes {
+		if v.MinAvailableCapacitySlackStreamingUnits < 0 {
+			return fmt.Errorf("kafka minimum available capacity slack for instance type '%s' in region '%s' cannot be negative", k, r.Name)
+		}
+
 		// skip if limit is not set or is explicitly set to 0
 		if v.Limit == nil || v.Limit != nil && *v.Limit == 0 {
 			continue
 		}
 
-		if len(r.SupportedInstanceTypes) == 1 {
-			capacity := dataplaneClusterConfig.ClusterConfig.GetCapacityForRegionAndInstanceType(r.Name, k, false)
-			if *v.Limit != capacity {
-				return fmt.Errorf("limit for instance type '%s'(%d) does not match the capacity in region %s(%d)", k, *v.Limit, r.Name, capacity)
+		if v.Limit != nil && v.MinAvailableCapacitySlackStreamingUnits > *v.Limit {
+			return fmt.Errorf("Configured kafka minimum available capacity slack '%d' for instance type '%s' in region '%s' cannot be bigger than its region limit '%v'", v.MinAvailableCapacitySlackStreamingUnits, k, r.Name, *v.Limit)
+		}
+
+		// validate instance type limits with the data plane cluster configuration when manual scaling is enabled
+		if dataplaneClusterConfig.IsDataPlaneManualScalingEnabled() {
+			if len(r.SupportedInstanceTypes) == 1 {
+				capacity := dataplaneClusterConfig.ClusterConfig.GetCapacityForRegionAndInstanceType(r.Name, k, false)
+				if *v.Limit != capacity {
+					return fmt.Errorf("limit for instance type '%s'(%d) does not match the capacity in region %s(%d)", k, *v.Limit, r.Name, capacity)
+				}
+				return nil
 			}
-			return nil
-		}
 
-		// ensure that limit is within min and max capacity
-		// min: the total capacity of clusters that support only this instance type
-		// max: the total capacity of clusters that supports this instance type
-		minCapacity := dataplaneClusterConfig.ClusterConfig.GetCapacityForRegionAndInstanceType(r.Name, k, true)
-		maxCapacity := dataplaneClusterConfig.ClusterConfig.GetCapacityForRegionAndInstanceType(r.Name, k, false)
-		if minCapacity > *v.Limit || maxCapacity < *v.Limit {
-			return fmt.Errorf("limit for %s instance type (%d) does not match cluster capacity configuration in region '%s': min(%d), max(%d)", k, *v.Limit, r.Name, minCapacity, maxCapacity)
-		}
-
-		// when all limits are set, ensure its total adds up to total capacity of the region.
-		if !r.RegionHasZeroOrNoLimitInstanceType() {
-			totalCapacityUsed += *v.Limit
-
-			// when we reach the last item, ensure limits for all instance types adds up to the total capacity of the region
-			if counter == len(r.SupportedInstanceTypes) && totalCapacityUsed != regionCapacity {
-				return fmt.Errorf("total limits set in region '%s' does not match cluster capacity configuration", r.Name)
+			// ensure that limit is within min and max capacity
+			// min: the total capacity of clusters that support only this instance type
+			// max: the total capacity of clusters that supports this instance type
+			minCapacity := dataplaneClusterConfig.ClusterConfig.GetCapacityForRegionAndInstanceType(r.Name, k, true)
+			maxCapacity := dataplaneClusterConfig.ClusterConfig.GetCapacityForRegionAndInstanceType(r.Name, k, false)
+			if minCapacity > *v.Limit || maxCapacity < *v.Limit {
+				return fmt.Errorf("limit for %s instance type (%d) does not match cluster capacity configuration in region '%s': min(%d), max(%d)", k, *v.Limit, r.Name, minCapacity, maxCapacity)
 			}
+
+			// when all limits are set, ensure its total adds up to total capacity of the region.
+			if !r.RegionHasZeroOrNoLimitInstanceType() {
+				totalCapacityUsed += *v.Limit
+
+				// when we reach the last item, ensure limits for all instance types adds up to the total capacity of the region
+				if counter == len(r.SupportedInstanceTypes) && totalCapacityUsed != regionCapacity {
+					return fmt.Errorf("total limits set in region '%s' does not match cluster capacity configuration", r.Name)
+				}
+			}
+			counter++
 		}
-		counter++
+
 	}
 
 	return nil
@@ -168,6 +186,7 @@ func NewSupportedProvidersConfig() *ProviderConfig {
 var _ environments.ServiceValidator = &ProviderConfig{}
 
 func (c *ProviderConfig) Validate(env *environments.Env) error {
+
 	var dataplaneClusterConfig *DataplaneClusterConfig
 	env.MustResolve(&dataplaneClusterConfig)
 
@@ -194,11 +213,8 @@ func (provider Provider) Validate(dataplaneClusterConfig *DataplaneClusterConfig
 			defaultCount++
 		}
 
-		// validate instance type limits with the data plane cluster configuration when manual scaling is enabled
-		if dataplaneClusterConfig.IsDataPlaneManualScalingEnabled() {
-			if err := p.Validate(dataplaneClusterConfig); err != nil {
-				return err
-			}
+		if err := p.Validate(dataplaneClusterConfig); err != nil {
+			return err
 		}
 	}
 	if defaultCount != 1 {
