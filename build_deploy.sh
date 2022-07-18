40b2c0305ae2d166d1b3a7aac887122a6be50cd9
Pawel Paszki <ppaszki@redhat.com>
2022-07-18 12:48:16 +0100

    feat: restored build script stuff pre-releae creation

--- build_deploy.sh
+++ build_deploy.sh
@@ -37,75 +37,75 @@ set -e
 # The version should be the short hash from git. This is what the deployent process expects.
 VERSION="$(git rev-parse --short=7 HEAD)"
 
-# # Set the variable required to login and push images to the registry
-# QUAY_USER=${QUAY_USER_NAME:-$RHOAS_QUAY_USER}
-# QUAY_TOKEN=${QUAY_USER_PASSWORD:-$RHOAS_QUAY_TOKEN}
-# QUAY_ORG=${QUAY_ORG_NAME:-rhoas}
+# Set the variable required to login and push images to the registry
+QUAY_USER=${QUAY_USER_NAME:-$RHOAS_QUAY_USER}
+QUAY_TOKEN=${QUAY_USER_PASSWORD:-$RHOAS_QUAY_TOKEN}
+QUAY_ORG=${QUAY_ORG_NAME:-rhoas}
 
-# # Set the directory for docker configuration:
-# DOCKER_CONFIG="${PWD}/.docker"
+# Set the directory for docker configuration:
+DOCKER_CONFIG="${PWD}/.docker"
 
-# # Set the Go path:
-# export GOPATH="${PWD}/.gopath"
-# export PATH="${PATH}:${GOPATH}/bin"
-# LINK="${GOPATH}/src/github.com/bf2fc6cc711aee1a0c2a/kas-fleet-manager"
+# Set the Go path:
+export GOPATH="${PWD}/.gopath"
+export PATH="${PATH}:${GOPATH}/bin"
+LINK="${GOPATH}/src/github.com/bf2fc6cc711aee1a0c2a/kas-fleet-manager"
 
-# # print go version
-# go version  
+# print go version
+go version  
 
-# mkdir -p "$(dirname "${LINK}")"
-# ln -sf "${PWD}" "${LINK}"
-# cd "${LINK}"
+mkdir -p "$(dirname "${LINK}")"
+ln -sf "${PWD}" "${LINK}"
+cd "${LINK}"
 
-# # Log in to the image registry:
-# if [ -z "${QUAY_USER}" ]; then
-#   echo "The quay.io push user name hasn't been provided."
-#   echo "Make sure to set the QUAY_USER_NAME environment variable."
-#   exit 1
-# fi
-# if [ -z "${QUAY_TOKEN}" ]; then
-#   echo "The quay.io push token hasn't been provided."
-#   echo "Make sure to set the QUAY_USER_PASSWORD environment variable."
-#   exit 1
-# fi
+# Log in to the image registry:
+if [ -z "${QUAY_USER}" ]; then
+  echo "The quay.io push user name hasn't been provided."
+  echo "Make sure to set the QUAY_USER_NAME environment variable."
+  exit 1
+fi
+if [ -z "${QUAY_TOKEN}" ]; then
+  echo "The quay.io push token hasn't been provided."
+  echo "Make sure to set the QUAY_USER_PASSWORD environment variable."
+  exit 1
+fi
 
-# # Set up the docker config directory
-# mkdir -p "${DOCKER_CONFIG}"
+# Set up the docker config directory
+mkdir -p "${DOCKER_CONFIG}"
 
-# BRANCH="main"
-# if [[ ! -z "$GITHUB_REF" ]]; then
-#   BRANCH="$(echo $GITHUB_REF | awk -F/ '{print $NF}')"
-#   echo "GITHUB_REF is defined. Set image tag to $BRANCH."
-# elif [[ ! -z "$GIT_BRANCH" ]]; then
-#   BRANCH="$(echo $GIT_BRANCH | awk -F/ '{print $NF}')"
-#   echo "GIT_BRANCH is defined. Set image tag to $BRANCH."
-# else
-#   echo "No git branch env var found. Set image tag to $BRANCH."
-# fi
+BRANCH="main"
+if [[ ! -z "$GITHUB_REF" ]]; then
+  BRANCH="$(echo $GITHUB_REF | awk -F/ '{print $NF}')"
+  echo "GITHUB_REF is defined. Set image tag to $BRANCH."
+elif [[ ! -z "$GIT_BRANCH" ]]; then
+  BRANCH="$(echo $GIT_BRANCH | awk -F/ '{print $NF}')"
+  echo "GIT_BRANCH is defined. Set image tag to $BRANCH."
+else
+  echo "No git branch env var found. Set image tag to $BRANCH."
+fi
 
-# # Push the image:
-# echo "Quay.io user and token is set, will push images to $QUAY_ORG org"
-# make \
-#   DOCKER_CONFIG="${DOCKER_CONFIG}" \
-#   QUAY_USER="${QUAY_USER}" \
-#   QUAY_TOKEN="${QUAY_TOKEN}" \
-#   version="${VERSION}" \
-#   external_image_registry="quay.io" \
-#   internal_image_registry="quay.io" \
-#   image_repository="$QUAY_ORG/kas-fleet-manager" \
-#   docker/login \
-#   image/push
+# Push the image:
+echo "Quay.io user and token is set, will push images to $QUAY_ORG org"
+make \
+  DOCKER_CONFIG="${DOCKER_CONFIG}" \
+  QUAY_USER="${QUAY_USER}" \
+  QUAY_TOKEN="${QUAY_TOKEN}" \
+  version="${VERSION}" \
+  external_image_registry="quay.io" \
+  internal_image_registry="quay.io" \
+  image_repository="$QUAY_ORG/kas-fleet-manager" \
+  docker/login \
+  image/push
 
-# make \
-#   DOCKER_CONFIG="${DOCKER_CONFIG}" \
-#   QUAY_USER="${QUAY_USER}" \
-#   QUAY_TOKEN="${QUAY_TOKEN}" \
-#   version="${BRANCH}" \
-#   external_image_registry="quay.io" \
-#   internal_image_registry="quay.io" \
-#   image_repository="$QUAY_ORG/kas-fleet-manager" \
-#   docker/login \
-#   image/push
+make \
+  DOCKER_CONFIG="${DOCKER_CONFIG}" \
+  QUAY_USER="${QUAY_USER}" \
+  QUAY_TOKEN="${QUAY_TOKEN}" \
+  version="${BRANCH}" \
+  external_image_registry="quay.io" \
+  internal_image_registry="quay.io" \
+  image_repository="$QUAY_ORG/kas-fleet-manager" \
+  docker/login \
+  image/push
 
 # create new relaase in managed-kafka-versions repo for kas-fleet-manager
 if [[ -n "$AUTHOR_EMAIL" ]] && [[ -n "$AUTHOR_NAME" ]] && [[ -n "$GITLAB_TOKEN" ]]; then
