8077fd91d2b34e910ebcc462b3cdaee4ef031112
Jameel Briones <jbriones@redhat.com>
2022-07-19 16:56:34 +0100

    test: add integration test for kafka create when manual scaling is enabled

--- internal/kafka/test/integration/kafka_create_test.go
+++ internal/kafka/test/integration/kafka_create_test.go
@@ -24,6 +24,217 @@ import (
 	"github.com/onsi/gomega"
 )
 
+// Test Kafka creation with manual scaling enabled
+func TestKafkaCreate_ManualScaling(t *testing.T) {
+	g := gomega.NewWithT(t)
+
+	ocmServer := mocks.NewMockConfigurableServerBuilder().Build()
+	defer ocmServer.Close()
+
+	var enableManualScaling bool
+	clusterList := config.ClusterList{}
+	h, client, teardown := kafkatest.NewKafkaHelperWithHooks(t, ocmServer, func(d *config.DataplaneClusterConfig) {
+		if enableManualScaling {
+			// Enable manual scaling and register data plane cluster to cluster config
+			d.DataPlaneClusterScalingType = config.ManualScaling
+			d.ClusterConfig = config.NewClusterConfig(clusterList)
+		}
+	})
+	defer teardown()
+
+	// run mock fleetshard sync
+	mockKasFleetshardSyncBuilder := kasfleetshardsync.NewMockKasFleetshardSyncBuilder(h, t)
+	mockKasFleetshardSync := mockKasFleetshardSyncBuilder.Build()
+	mockKasFleetshardSync.Start()
+	defer mockKasFleetshardSync.Stop()
+
+	// set up data plane cluster
+	clusterID, getClusterErr := common.GetOSDClusterIDAndWaitForStatus(h, t, api.ClusterReady)
+	g.Expect(getClusterErr).ToNot(gomega.HaveOccurred(), "failed to get dataplane cluster details: %v", getClusterErr)
+	g.Expect(clusterID).ToNot(gomega.BeEmpty(), "no dataplane cluster found")
+
+	// reload services with manual scaling enabled
+	h.Env.Stop()
+
+	// register existing cluster to the data plane cluster config
+	db := h.DBFactory().New()
+	cluster := api.Cluster{
+		ClusterID: clusterID,
+	}
+	err := db.First(&cluster).Error
+	g.Expect(err).ToNot(gomega.HaveOccurred(), "failed to get data plane cluster from database: %v", err)
+
+	clusterList = append(clusterList, config.ManualCluster{
+		Name:                  "test-cluster",
+		ClusterId:             cluster.ClusterID,
+		CloudProvider:         cluster.CloudProvider,
+		Region:                cluster.Region,
+		MultiAZ:               cluster.MultiAZ,
+		Schedulable:           true,
+		KafkaInstanceLimit:    2,
+		Status:                cluster.Status,
+		SupportedInstanceType: cluster.SupportedInstanceType,
+	})
+	enableManualScaling = true
+
+	err = h.Env.CreateServices()
+	g.Expect(err).ToNot(gomega.HaveOccurred(), "unable to initialize testing environment: %v", err)
+
+	h.Env.Start()
+
+	// setup pre-requisites for performing requests
+	account := h.NewRandAccount()
+	ctx := h.NewAuthenticatedContext(account, nil)
+
+	account2 := h.NewAccount("test-user-2", "", "", "test-org-2")
+
+	kafkaRequestPayload := public.KafkaRequestPayload{
+		Region:        mocks.MockCluster.Region().ID(),
+		CloudProvider: mocks.MockCluster.CloudProvider().ID(),
+		Name:          mockKafkaName,
+		Plan:          fmt.Sprintf("%s.x1", types.STANDARD.String()),
+	}
+
+	testCases := []struct {
+		name    string
+		setup   func()
+		assert  func()
+		cleanup func()
+	}{
+		{
+			name: "should successfully create kafka",
+			assert: func() {
+				kafka, resp, err := common.WaitForKafkaCreateToBeAccepted(ctx, kafkatest.TestServices.DBFactory, client, kafkaRequestPayload)
+				if resp != nil {
+					resp.Body.Close()
+				}
+				// kafka successfully registered with database
+				g.Expect(err).NotTo(gomega.HaveOccurred(), "Error posting object:  %v", err)
+				g.Expect(resp.StatusCode).To(gomega.Equal(http.StatusAccepted))
+				g.Expect(kafka.Id).NotTo(gomega.BeEmpty(), "Expected ID assigned on creation")
+				g.Expect(kafka.Owner).To(gomega.Equal(account.Username()))
+				g.Expect(kafka.Kind).To(gomega.Equal(presenters.KindKafka))
+				g.Expect(kafka.Href).To(gomega.Equal(fmt.Sprintf("/api/kafkas_mgmt/v1/kafkas/%s", kafka.Id)))
+				g.Expect(kafka.InstanceType).To(gomega.Equal(types.STANDARD.String()))
+				g.Expect(kafka.DeprecatedInstanceTypeName).To(gomega.Equal("Standard"))
+				g.Expect(kafka.ReauthenticationEnabled).To(gomega.BeTrue())
+				g.Expect(kafka.BrowserUrl).To(gomega.Equal(fmt.Sprintf("%s%s/dashboard", kafkatest.TestServices.KafkaConfig.BrowserUrl, kafka.Id)))
+				g.Expect(kafka.ExpiresAt).To(gomega.BeNil())
+				g.Expect(kafka.AdminApiServerUrl).To(gomega.BeEmpty())
+
+				// wait until the kafka goes into a ready state
+				// the timeout here assumes a backing cluster has already been provisioned
+				readyKafka, err := common.WaitForKafkaToReachStatus(ctx, kafkatest.TestServices.DBFactory, client, kafka.Id, constants.KafkaRequestStatusReady)
+				g.Expect(err).NotTo(gomega.HaveOccurred(), "Error waiting for kafka request to become ready: %v", err)
+				g.Expect(readyKafka.BootstrapServerHost).ToNot(gomega.BeEmpty())
+				g.Expect(strings.HasSuffix(readyKafka.BootstrapServerHost, ":443")).To(gomega.Equal(true))
+				g.Expect(readyKafka.Version).To(gomega.Equal(kasfleetshardsync.GetDefaultReportedKafkaVersion()))
+				g.Expect(readyKafka.AdminApiServerUrl).To(gomega.Equal(kasfleetshardsync.AdminServerURI))
+
+				// default kafka max data retention size should be set on creation
+				instanceType, err := kafkatest.TestServices.KafkaConfig.SupportedInstanceTypes.Configuration.GetKafkaInstanceTypeByID(readyKafka.InstanceType)
+				g.Expect(err).ToNot(gomega.HaveOccurred(), "failed to get kafka instance type by id")
+
+				instanceSize, err := instanceType.GetKafkaInstanceSizeByID(kafka.SizeId)
+				g.Expect(err).ToNot(gomega.HaveOccurred(), "failed to get kafka instance size by id")
+				g.Expect(readyKafka.DeprecatedKafkaStorageSize).To(gomega.Equal(instanceSize.MaxDataRetentionSize.String()))
+
+				maxDataRetentionSizeBytes, err := instanceSize.MaxDataRetentionSize.ToInt64()
+				g.Expect(err).ToNot(gomega.HaveOccurred(), "failed to convert max data retention size to bytes")
+				g.Expect(readyKafka.MaxDataRetentionSize.Bytes).To(gomega.Equal(maxDataRetentionSizeBytes))
+
+				// check kafka details that's not included in the public kafka request representation
+				db := h.DBFactory().New()
+				var kafkaRequest dbapi.KafkaRequest
+				err = db.Unscoped().Where("id = ?", kafka.Id).First(&kafkaRequest).Error
+				g.Expect(err).ToNot(gomega.HaveOccurred(), "failed to find newly created kafka in the database")
+
+				g.Expect(kafkaRequest.QuotaType).To(gomega.Equal(KafkaConfig(h).Quota.Type))
+				g.Expect(kafkaRequest.PlacementId).ToNot(gomega.BeEmpty())
+				g.Expect(kafkaRequest.Namespace).To(gomega.Equal(fmt.Sprintf("kafka-%s", strings.ToLower(kafkaRequest.ID))))
+				// this is set by the mockKasfFleetshardSync
+				g.Expect(kafkaRequest.DesiredStrimziVersion).To(gomega.Equal(kasfleetshardsync.GetDefaultReportedStrimziVersion()))
+
+				common.CheckMetricExposed(h, t, metrics.KafkaCreateRequestDuration)
+				common.CheckMetricExposed(h, t, fmt.Sprintf("%s_%s{operation=\"%s\"} 1", metrics.KasFleetManager, metrics.KafkaOperationsSuccessCount, constants2.KafkaOperationCreate.String()))
+				common.CheckMetricExposed(h, t, fmt.Sprintf("%s_%s{operation=\"%s\"} 1", metrics.KasFleetManager, metrics.KafkaOperationsTotalCount, constants2.KafkaOperationCreate.String()))
+			},
+			cleanup: func() {
+				// delete test kafka to free up resources on the OSD cluster
+				db := h.DBFactory().New().Model(&dbapi.KafkaRequest{})
+				var kafkas []*dbapi.KafkaRequest
+				err := db.Scan(&kafkas).Error
+				g.Expect(err).ToNot(gomega.HaveOccurred(), "failed to get all kafka in the database")
+
+				for _, kafka := range kafkas {
+					deleteTestKafka(t, h, ctx, client, kafka.ID)
+				}
+			},
+		},
+		{
+			name: "should reject kafka request if there is no capacity left",
+			setup: func() {
+				// Create dummy Kafkas to fill up the cluster capacity
+				dummyKafkas := []*dbapi.KafkaRequest{}
+				for i := 1; i <= int(clusterList[0].KafkaInstanceLimit); i++ {
+					dummyKafkas = append(dummyKafkas, mockkafkas.BuildKafkaRequest(
+						mockkafkas.WithPredefinedTestValues(),
+						mockkafkas.With(mockkafkas.NAME, fmt.Sprintf("dummy-kafka-%d", i)),
+						mockkafkas.With(mockkafkas.CLUSTER_ID, clusterID),
+						mockkafkas.With(mockkafkas.CLOUD_PROVIDER, kafkaRequestPayload.CloudProvider),
+						mockkafkas.With(mockkafkas.REGION, kafkaRequestPayload.Region),
+						mockkafkas.With(mockkafkas.OWNER, account2.Username()),
+						mockkafkas.With(mockkafkas.ORGANISATION_ID, account2.Organization().ExternalID()),
+						mockkafkas.With(mockkafkas.INSTANCE_TYPE, types.STANDARD.String()),
+						mockkafkas.With(mockkafkas.SIZE_ID, "x1"),
+					))
+				}
+
+				db := h.DBFactory().New()
+				err := db.Create(&dummyKafkas).Error
+				g.Expect(err).NotTo(gomega.HaveOccurred(), "failed to create dummy kafkas")
+			},
+			assert: func() {
+				_, resp, err := common.WaitForKafkaCreateToBeAccepted(ctx, kafkatest.TestServices.DBFactory, client, kafkaRequestPayload)
+				if resp != nil {
+					resp.Body.Close()
+				}
+				g.Expect(err).NotTo(gomega.HaveOccurred(), "Error posting object:  %v", err)
+				// get a 403 status code as there is no capacity left
+				g.Expect(resp.StatusCode).To(gomega.Equal(http.StatusForbidden), "kafka should have been rejected as no capacity is left")
+			},
+			cleanup: func() {
+				// delete dummy kafkas to free up resources on the OSD cluster
+				db := h.DBFactory().DB.Model(&dbapi.KafkaRequest{})
+				var kafkas []*dbapi.KafkaRequest
+				err := db.Scan(&kafkas).Error
+				g.Expect(err).ToNot(gomega.HaveOccurred(), "failed to get all kafka in the database")
+
+				ctx := h.NewAuthenticatedContext(account2, nil)
+
+				for _, kafka := range kafkas {
+					deleteTestKafka(t, h, ctx, client, kafka.ID)
+				}
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		testcase := tc
+		t.Run(testcase.name, func(t *testing.T) {
+			if testcase.setup != nil {
+				testcase.setup()
+			}
+
+			testcase.assert()
+
+			if testcase.cleanup != nil {
+				testcase.cleanup()
+			}
+		})
+	}
+}
+
 // Test Kafka creation with dynamic scaling enabled
 func TestKafkaCreate_DynamicScaling(t *testing.T) {
 	g := gomega.NewWithT(t)
