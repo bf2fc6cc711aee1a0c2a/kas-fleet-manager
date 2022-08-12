a37cab5ab0811eba1a125ae3449786ee91222d46
Luca Burgazzoli <lburgazzoli@gmail.com>
2022-08-12 14:15:31 +0200

    chore: make slices with know len

--- test/cucumber/sql.go
+++ test/cucumber/sql.go
@@ -109,8 +109,8 @@ func (s *TestScenario) iRunSQLGivesResults(sql string, expected *godog.Table) er
 		actual := StringTableToCucumberTable(actualTable)
 
 		diff, _ := difflib.GetUnifiedDiffString(difflib.UnifiedDiff{
-			A:        difflib.SplitLines(string(expected)),
-			B:        difflib.SplitLines(string(actual)),
+			A:        difflib.SplitLines(expected),
+			B:        difflib.SplitLines(actual),
 			FromFile: "Expected",
 			FromDate: "",
 			ToFile:   "Actual",
@@ -123,13 +123,12 @@ func (s *TestScenario) iRunSQLGivesResults(sql string, expected *godog.Table) er
 }
 
 func GodogTableToStringTable(table *godog.Table) [][]string {
-	data := [][]string{}
-	for _, row := range table.Rows {
-		dataR := []string{}
-		for _, c := range row.Cells {
-			dataR = append(dataR, c.Value)
+	data := make([][]string, len(table.Rows))
+	for r, row := range table.Rows {
+		data[r] = make([]string, len(row.Cells))
+		for c, cell := range row.Cells {
+			data[r][c] = cell.Value
 		}
-		data = append(data, dataR)
 	}
 	return data
 }
