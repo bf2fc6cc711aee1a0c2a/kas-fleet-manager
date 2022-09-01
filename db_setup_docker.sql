f42a96e031bc0d42f06890b0f4f9452ea49789aa
Jameel Briones <jbriones@redhat.com>
2022-09-01 15:55:45 +0100

    ci: set postgres max_connections to 200 in Dockerfile templates

--- db_setup_docker.sql
+++ db_setup_docker.sql
@@ -1,3 +1,4 @@
 CREATE USER postgres with password 'postgres';
 GRANT ALL PRIVILEGES ON DATABASE serviceapitests TO postgres;
 GRANT ALL PRIVILEGES ON DATABASE serviceapitests TO kas_fleet_manager;
+ALTER SYSTEM SET max_connections=200;
