7ade896398fb40d4ffc9108ed504603badb87366
Luis Garcia Acosta <lgarciaa@redhat.com>
2022-09-09 10:18:02 +0200

    [chore] fixes to openapi.yaml

--- internal/connector/internal/api/admin/private/model_connector_request.go
+++ internal/connector/internal/api/admin/private/model_connector_request.go
@@ -11,5 +11,13 @@ package private
 
 // ConnectorRequest struct for ConnectorRequest
 type ConnectorRequest struct {
-	DesiredState ConnectorDesiredState `json:"desired_state"`
+	Name            string                           `json:"name"`
+	ConnectorTypeId string                           `json:"connector_type_id"`
+	NamespaceId     string                           `json:"namespace_id"`
+	Channel         Channel                          `json:"channel,omitempty"`
+	DesiredState    ConnectorDesiredState            `json:"desired_state"`
+	Kafka           KafkaConnectionSettings          `json:"kafka"`
+	ServiceAccount  ServiceAccount                   `json:"service_account"`
+	SchemaRegistry  SchemaRegistryConnectionSettings `json:"schema_registry,omitempty"`
+	Connector       map[string]interface{}           `json:"connector"`
 }
