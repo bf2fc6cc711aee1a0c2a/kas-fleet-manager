eebd1b4af80882f3d0f92957505c5f839813b798
Miguel Soriano <msoriano@redhat.com>
2023-01-05 17:33:11 +0100

    feat: prevent resuming long lived kafka instances on grace period
    Also fixes an issue that allowed resuming kafka instances no matter
    the state they were

--- internal/kafka/internal/routes/route_loader.go
+++ internal/kafka/internal/routes/route_loader.go
@@ -214,6 +214,23 @@ func (s *options) buildApiBaseRouter(mainRouter *mux.Router, basePath string) er
 		Name(logger.NewLogEvent("list-regions", "list cloud provider regions").ToString()).
 		Methods(http.MethodGet)
 
+	v1Metadata := api.VersionMetadata{
+		ID:          "v1",
+		Collections: v1Collections,
+	}
+	apiMetadata := api.Metadata{
+		ID: "kafkas_mgmt",
+		Versions: []api.VersionMetadata{
+			v1Metadata,
+		},
+	}
+	apiRouter.HandleFunc("", apiMetadata.ServeHTTP).Methods(http.MethodGet)
+	apiRouter.Use(coreHandlers.MetricsMiddleware)
+	apiRouter.Use(db.TransactionMiddleware(s.DB))
+	apiRouter.Use(gorillaHandlers.CompressHandler)
+
+	apiV1Router.HandleFunc("", v1Metadata.ServeHTTP).Methods(http.MethodGet)
+
 	// /api/kafkas_mgmt/v1/instance_types/{cloud_provider}/{cloud_region}
 	apiV1SupportedKafkaInstanceTypesRouter := apiV1Router.PathPrefix("/instance_types/{cloud_provider}/{cloud_region}").Subrouter()
 	apiV1SupportedKafkaInstanceTypesRouter.HandleFunc("", supportedKafkaInstanceTypesHandler.ListSupportedKafkaInstanceTypes).
@@ -275,7 +292,7 @@ func (s *options) buildApiBaseRouter(mainRouter *mux.Router, basePath string) er
 	observatoriumProxyRouter.Use(auth.NewRequireIssuerMiddleware().RequireIssuer([]string{s.Keycloak.GetRealmConfig().ValidIssuerURI}, errors.ErrorNotFound))
 
 	// /api/kafkas_mgmt/v1/admin/kafkas
-	adminKafkaHandler := handlers.NewAdminKafkaHandler(s.Kafka, s.AccountService, s.ProviderConfig, s.ClusterService)
+	adminKafkaHandler := handlers.NewAdminKafkaHandler(s.Kafka, s.AccountService, s.ProviderConfig, s.ClusterService, s.KafkaConfig)
 	adminRouter := apiV1Router.PathPrefix("/admin").Subrouter()
 	adminRouter.Use(auth.NewRequireIssuerMiddleware().RequireIssuer([]string{s.Keycloak.GetConfig().AdminAPISSORealm.ValidIssuerURI}, errors.ErrorNotFound))
 	adminRouter.Use(auth.NewRolesAuthzMiddleware(s.AdminRoleAuthZConfig).RequireRolesForMethods(errors.ErrorNotFound))
