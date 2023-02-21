eec0ec68664baf9abe8b3bed9cb7c99b9952daab
Michael Edgar <medgar@redhat.com>
2023-02-21 15:36:17 -0500

    fix: Add TypeMeta to observability-proxy-credentials secret
    Signed-off-by: Michael Edgar <medgar@redhat.com>

--- internal/kafka/internal/workers/cluster_mgrs/clusters_mgr.go
+++ internal/kafka/internal/workers/cluster_mgrs/clusters_mgr.go
@@ -881,6 +881,10 @@ func (c *ClusterManager) buildObservatoriumDexSecretResource() *k8sCoreV1.Secret
 
 func (c *ClusterManager) buildObservabilityRemoteWriteServiceAccountCredential(cluster *api.Cluster) *k8sCoreV1.Secret {
 	return &k8sCoreV1.Secret{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: k8sCoreV1.SchemeGroupVersion.String(),
+			Kind:       "Secret",
+		},
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      "observability-proxy-credentials",
 			Namespace: observabilityNamespace,
